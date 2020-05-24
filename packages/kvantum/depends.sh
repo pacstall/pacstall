6f07fd1ce6d623e518997e835118e4512c2cdc50
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-24 18:41:55 -0500

    Update depends.sh
    Former-commit-id: 45193d071d08f23036b9287a8a85a6a64f8b614e

--- packages/kvantum/depends.sh
+++ packages/kvantum/depends.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-sudo apt install -y qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev libkf5windowsystem-dev
+value=$( command -v apt )
+if [ $value -eq /usr/bin/apt ]
+then
+  echo "Debian based system recognized, installing dependencies"
+  sudo apt install -y qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev libkf5windowsystem-dev
+else
+  echo "Debian based system not recognized... Trying Arch based"
+  value=$( command -v pacman )
+  if [ $value -eq /usr/bin/pacman ]
+  then
+    echo "Arch based system recognized, installing dependencies"
+    sudo pacman -S qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev libkf5windowsystem-dev
+  fi
+fi
