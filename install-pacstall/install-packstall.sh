9d74f896b92802868451a779817d49796b8254c8
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-24 16:24:27 -0500

    Add files via upload
    Former-commit-id: c20db1ace849455ca56c7c1477e47ada7adc0ea4
    Former-commit-id: e63b8a919f7acdbd195633c4eb842d3a1de50fb9

---
+++ install-pacstall/install-packstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo mkdir /usr/share/pacstall/
+sudo echo Henryws > /usr/share/pacstall/repo/pacstallrepo.txt
+sudo wget -P /bin https://raw.githubusercontent.com/Henryws/test-pacstall/master/pacstall
