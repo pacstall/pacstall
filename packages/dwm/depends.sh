b774f929076e6f34fb28988c49973d8aca3f56e0
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-25 16:17:07 -0500

    Update depends.sh
    Former-commit-id: 6cf0b147719316f0716fab3a082144aab0bd0d66

--- packages/dwm/depends.sh
+++ packages/dwm/depends.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-if [ command -v pacman -eq /usr/bin/pacmna ]
+if [ command -v pacman -eq /usr/bin/pacman ]
 then
 sudo pacman -S dmenu freetype2 st libxinerama
 fi
 if [ command -v apt -eq /usr/bin/apt ]
 then
 sudo apt install -y dmenu freetype2 libxinerama
+fi
 git clone http://git.suckless.org/st
 cd st
 sudo make install
-fi
+
