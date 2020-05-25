577fab6a168aba4fea69451902fc86c48ba039ee
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-25 10:38:14 -0500

    Create install.sh
    Former-commit-id: 7637fd0ce46024f1dfb02ea1c0442cabd6b28883

---
+++ packages/conky/install.sh
@@ -0,0 +1,8 @@
+mkdir build && cd build
+make
+if checkinstall ; then
+    echo "checkinstall succeded... Installing"
+else
+    echo "checkinstall failed... running make install"
+    make install
+fi
