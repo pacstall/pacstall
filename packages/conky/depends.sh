0b651e69c7c79a1a19964af7ccb6b53cf67db2d4
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-25 12:16:08 -0500

    Update depends.sh
    Former-commit-id: a35cb57a0b023ea396359bf7e851f0c6526efbe3
    Former-commit-id: b6b9fb6d2ec3561fb3a5fcf652bb3823a2d096e8

--- packages/conky/depends.sh
+++ packages/conky/depends.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ command -v apt -eq /usr/bin/apt ]
 then
     sudo apt-get install -y cmake libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev
