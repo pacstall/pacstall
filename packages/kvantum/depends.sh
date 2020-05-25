3af88a4ada68a2ef5a4015083edd116c9dbc45aa
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-25 09:09:11 -0500

    Update depends.sh

--- packages/kvantum/depends.sh
+++ packages/kvantum/depends.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-value=$( command -v apt )
-if [ $value -eq /usr/bin/apt ]
+if [ command -v apt -eq /usr/bin/apt ]
 then
-  echo "Debian based system recognized, installing dependencies"
-  sudo apt install -y qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev libkf5windowsystem-dev
-else
-  echo "Debian based system not recognized... Trying Arch based"
-  value=$( command -v pacman )
-  if [ $value -eq /usr/bin/pacman ]
-  then
-    echo "Arch based system recognized, installing dependencies"
-    sudo pacman -S qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev libkf5windowsystem-dev
-  fi
+sudo apt install -y qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev
+fi
+if [ command -v pacman -eq /usr/bin/pacman ]
+then
+sudo pacman -S qt5-base qt5-svg qt5-x11extras
 fi
