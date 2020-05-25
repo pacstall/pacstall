7c0323e11428eb7790615d104be654854a14d6b8
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-25 12:39:52 -0500

    Update install.sh

--- packages/conky/install.sh
+++ packages/conky/install.sh
@@ -1,7 +1,3 @@
+#!/bin/bash
 make
-if checkinstall ; then
-    echo "checkinstall succeded... Installing"
-else
-    echo "checkinstall failed... running make install"
-    make install
-fi
+make install
