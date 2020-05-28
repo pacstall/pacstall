12ad77b5aaaeefaf99928754c61b77fb168b616c
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-28 13:17:32 -0500

    Update depends.sh
    Former-commit-id: 5560db5cbe06f96a87c3fd7661087bcab615e90a

--- packages/conky/depends.sh
+++ packages/conky/depends.sh
@@ -3,3 +3,7 @@ equals=$(command -v apt)
 if [ $equals == /usr/bin/apt ] ; then
     sudo apt-get install -y cmake libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev
 fi
+equals=$(command -v dnf)
+if [ $equals == /usr/bin/dnf ] ; then
+    sudo dnf install -y cmake imlib2
+fi
