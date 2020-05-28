3d08da40035d33024e9c5ea205ce1e037be67d3d
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-28 18:38:37 -0500

    Update depends.sh

--- packages/gnome-tweaks/depends.sh
+++ packages/gnome-tweaks/depends.sh
@@ -7,4 +7,4 @@ equals=$(command -v dnf)
 if [ $equals == /usr/bin/dnf ] ; then
     sudo dnf install -y python3-pip ninja-build python27 python3-setuptools
 fi
-pip3 install --user meson
+pip3 install meson
