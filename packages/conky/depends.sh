62930d0d7140995ff4766bb517c1a57f1d6be3c8
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-30 14:00:58 -0500

    Update depends.sh
    Former-commit-id: 7bf8dda5ab53bebfe322e799cce9bf370793cda0
    Former-commit-id: 270d666126d99f9175319224eb6c5566f52cd5b7

--- packages/conky/depends.sh
+++ packages/conky/depends.sh
@@ -7,3 +7,12 @@ equals=$(command -v dnf)
 if [ $equals = /usr/bin/dnf ] ; then
     sudo dnf install -y cmake imlib2 libcurl libX11 libXdamage glibc cairo libcurl libgcc lua-libs g++ ncurses-devel libx11-devel libXdamage-devel libXft-devel libXinerama-devel libcurl-devel lua-devel imlib2-devel
 fi
+#Makes dependencylist variable which should print when asked if the user wishes to install program
+equals=$(command -v apt)
+if [ $equals = /usr/bin/apt ] ; then
+    dependencylist="cmake\nlibimlib2-dev\nlibncurses5-dev\nlibx11-dev\nlibxdamage-dev\nlibxft-dev\nlibxinerama-dev\nlibxml2-dev\nlibxext-dev\nlibcurl4-openssl-dev\nliblua5.3-dev\n"
+fi
+equals=$(command -v dnf)
+if [ $equals = /usr/bin/dnf ] ; then
+    dependencylist="cmake\nimlib2\nlibcurl\nlibX11\nlibXdamage\nglibc\ncairo\nlibcurl\nlibgcc\nlua-libs\ng++\nncurses-devel\nlibx11-devel\nlibXdamage-devel\nlibXft-devel\nlibXinerama-devel\nlibcurl-devel\nlua-devel\nimlib2-devel\n"
+fi
