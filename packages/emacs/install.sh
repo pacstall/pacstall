41c56b9c9bf91ab5fe49c8815677469b0c8d4174
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-03 10:28:52 -0500

    Update install.sh
    Former-commit-id: c3c768e9daa9eec4dc183b5ea7911be0630b4e07
    Former-commit-id: ed4cfad61e41d5428d9c6d811c655bdf896e341d

--- packages/emacs/install.sh
+++ packages/emacs/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://mirrors.ocf.berkeley.edu/gnu/emacs/emacs-26.3.tar.xz
-tar xvfz emacs-26.3.tar.xz
+wget -O emacs.tar.xz https://mirrors.ocf.berkeley.edu/gnu/emacs/emacs-26.3.tar.xz
+tar xvfz emacs.tar.xz
 cd emacs-26.3
 ./configure
 make
