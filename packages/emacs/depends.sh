558eae8356266521acd936837a0747adc3e2efab
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-03 10:57:34 -0500

    Update depends.sh
    Former-commit-id: 2ea8f3c70825f1913f11a1949e6eafd210f8eba8

--- packages/emacs/depends.sh
+++ packages/emacs/depends.sh
@@ -5,5 +5,5 @@ sudo apt install -y build-essential build-dep emacs24
 fi
 if [ command -v dnf -eq /usr/bin/dnf ]
 then
-sudo dnf install -y emacs-common liblockfile lobotf
+sudo dnf install -y emacs-common liblockfile lobotf Xaw3d libpng-devel libtiff-devel openjpeg-devel gtk2-devel ncurses-devel giflib-devel libX11-devel libXpm-devel libjpeg-devel gnutls-devel
 fi
