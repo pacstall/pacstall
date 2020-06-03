52f412e6379cddb54f5aa2797d6a3c8ad2076d2c
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-03 12:05:21 -0500

    Create install.sh

---
+++ packages/nano/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./configure
+make
+if sudo checkinstall ; then
+    echo "checkinstall succeeded"
+else
+    echo "checkinstall failed... running make install"
+    sudo make install
+fi
