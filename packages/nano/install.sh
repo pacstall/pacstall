f454570493fe8821faf0672481fa4bdbe3141d12
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-04 14:04:19 -0500

    Update install.sh
    Former-commit-id: eb2536b672077763f931cb5e7ddb713b437d695c [formerly b40dcd0125ea8b836cb320c7718ee8f18d9ebdb7] [formerly 7b2ce11ccda1899072ff39831eb15a1b597e8312]
    Former-commit-id: f73fe31ea7b84546dacdaf1efa82169170a51842
    Former-commit-id: 586706abc193c47cc4b60ce382ae654c5687e7cc

--- packages/nano/install.sh
+++ packages/nano/install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 ./configure
 make
-if sudo checkinstall ; then
-    echo "checkinstall succeeded"
-else
-    echo "checkinstall failed... running make install"
-    sudo make install
-fi
+sudo make install
