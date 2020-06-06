7efdc55a86ecf81dfd0048e2321d563fc013ca63
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-06 10:22:45 -0500

    Update depends.sh
    Former-commit-id: 88f8ebfe35bc9f06d36e5cf160cb16484e3d9e28
    Former-commit-id: 4dd74d87bae7f427369d49610ba4fe087d74fd11

--- packages/kvantum/depends.sh
+++ packages/kvantum/depends.sh
@@ -4,6 +4,7 @@ if [[ equals = /usr/bin/apt ]]
 then
 sudo apt install -y qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev
 fi
+equals=$(command -v dnf)
 if [[ $equals = /usr/bin/dnf ]]
 then
 sudo dnf install -y gcc-c++ libX11-devel libXext-devel qt5-qtx11extras-devel qt5-qtbase-devel qt5-qtsvg-devel qt5-qttools-devel kf5-kwindowsystem-devel qt5-qtbase-devel qt5-qtsvg-devel qt5-qtx11extras-devel kf5-kwindowsystem-devel
