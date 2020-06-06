8c6ba4a9bcfb68678db07e57d6fb932bd5c33aee
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-06 10:21:07 -0500

    Update depends.sh
    Former-commit-id: d8e725fbfaae29c45b835ff280f8b30ea401f560 [formerly 49e9d887065ddbf57381a1be59cf4d6f8a2e38a5] [formerly b9677979d5c81007dbdb519bae7c0525fced0b69]
    Former-commit-id: 55399e53063e3eb309b93eeb2966fa3a694ac3a6
    Former-commit-id: c5dbc6da3cd6e7ecf48a61f06488f19a333d14d0

--- packages/dwm/depends.sh
+++ packages/dwm/depends.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ command -v pacman -eq /usr/bin/pacman ]
+if [[ command -v pacman = /usr/bin/pacman ]]
 then
 sudo pacman -S dmenu freetype2 st libxinerama
 fi
-if [ command -v apt -eq /usr/bin/apt ]
+if [[ command -v apt = /usr/bin/apt ]]
 then
 sudo apt install -y dmenu freetype2 libxinerama
 fi
