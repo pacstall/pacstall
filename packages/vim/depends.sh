fef626470e6b92e46a578467a790b51980768911
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-06 10:19:55 -0500

    Update depends.sh

--- packages/vim/depends.sh
+++ packages/vim/depends.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ command -v apt -eq /usr/bin/apt ]
+if [[ command -v apt = /usr/bin/apt ]]
 then
 sudo apt install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
   libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
   libcairo2-dev libx11-dev libxpm-dev libxt-dev
 fi
-if [ command -v dnf -eq /usr/bin/dnf ]
+if [[ command -v dnf = /usr/bin/dnf ]]
 then
 sudo dnf install -y vim-common
 fi
