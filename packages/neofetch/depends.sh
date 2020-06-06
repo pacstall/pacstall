3da458e22e16b1c1972e7df159426ec9b1ea619c
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-06 10:18:55 -0500

    Update depends.sh

--- packages/neofetch/depends.sh
+++ packages/neofetch/depends.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 equals=$(command -v apt)
-if [ $equals == /usr/bin/apt ] ; then
+if [[ $equals = /usr/bin/apt ]] ; then
     sudo apt install -y w3m-img libsixel termpix pixterm catimg jp2a caca-utils libcaca
 fi
 equals=$(command -v dnf)
-if [ $equals == /usr/bin/dnf ] ; then
+if [[ $equals = /usr/bin/dnf ]] ; then
     sudo dnf install -y w3m-img caca-utils libcaca
 fi
