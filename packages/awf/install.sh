b0d8c31de3a140fba1265fef51d12bbadebcd335
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-08 12:29:13 -0500

    Update install.sh
    Former-commit-id: c382f2f95e4ccaeac2b5cd19a17782d4b70c7a55

--- packages/awf/install.sh
+++ packages/awf/install.sh
@@ -2,9 +2,4 @@
 ./autogen.sh
 ./configure
 make
-if sudo checkinstall ; then
-    echo "checkinstall succeeded"
-else
-    echo "checkinstall failed... running make install"
-    sudo make install
-fi
+paco -lp awf "sudo make install"
