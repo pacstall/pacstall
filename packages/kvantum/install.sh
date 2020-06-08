19ec1f0b52d822ded9506122c72444e8ba3ac042
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-08 12:47:52 -0500

    Update install.sh
    Former-commit-id: 20e1706736da3e7d23f38816b9e1b3302cd601f6 [formerly 7ccc1591924fab3978938755b648b6c9e6f2508c] [formerly a1fd716e725bbd16063800b6c2ac55c919b9b740]
    Former-commit-id: a84b19860c3c324e3db945dc1d7219dc8f7aa0f3
    Former-commit-id: 9125e8050f0aea1ac339b721c4a57a7a00817fcd

--- packages/kvantum/install.sh
+++ packages/kvantum/install.sh
@@ -2,9 +2,4 @@
 mkdir build && cd build
 cmake ..
 make
-if sudo checkinstall ; then
-    echo "checkinstall succeeded"
-else
-    echo "checkinstall failed... running make install"
-    sudo make install
-fi
+paco -lp kvantum "sudo make install"
