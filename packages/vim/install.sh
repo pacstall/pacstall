7f053d2cf8858f90aef78c6862b237dbe114a57b
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-08 12:59:08 -0500

    Update install.sh
    Former-commit-id: 364df4519d8ce34e0408ab4c18fb4e9016610492 [formerly 6e8b07ff8d94e6695701c7f69d480756edd6dfd1] [formerly 7b8b1f6588749541a48f38dc5b36f5ba489e0e9c]
    Former-commit-id: 882148265feaa1f25757dfe25ef6a087a7e67f01
    Former-commit-id: 1ed3431de17810d57b5d0ec5f51d6e7ea3c84eee

--- packages/vim/install.sh
+++ packages/vim/install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 make
-if sudo checkinstall ; then
-    echo "checkinstall succeeded"
-else
-    echo "checkinstall failed... running make install"
-    sudo make install
-fi
+paco -lp vim "sudo make install"
