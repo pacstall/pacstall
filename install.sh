0451d197536c8a3c1934e1fe6c82e9dbac9cae8a
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-12 19:04:57 -0500

    Update install.sh
    Former-commit-id: 9546dd402e2930feb643386a36e985cd94747b60
    Former-commit-id: f0dc885a6ac8e596773b9352e8589a051ceb681f

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Pacstall installer"
 sudo wget -q https://raw.githubusercontent.com/Henryws/pacstall/master/pacstall -O /bin/pacstall
-if [[ "command -v apt == /usr/bin/apt" ]] ; then
+equals=$(command -v apt)
+if [[ $equals = /usr/bin/apt ]]
+then
   sudo apt install -y wget dialog porg curl pandoc
 else
   sudo dnf install -y wget porg dialog curl pandoc
