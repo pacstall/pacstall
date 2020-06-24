2382ac3c03b0f692707254453f29a7424cea7248
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-24 17:44:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 echo "Pacstall installer"
-sudo wget -q https://raw.githubusercontent.com/Henryws/pacstall/master/pacstall -O /bin/pacstall
 equals=$(command -v apt)
 if [[ $equals = /usr/bin/apt ]]
 then
   sudo apt install -y wget dialog porg curl pandoc
+  sudo wget -q https://raw.githubusercontent.com/Henryws/pacstall/master/pacstall -O /bin/pacstall
 else
   sudo dnf install -y wget porg dialog curl pandoc
+  sudo wget -q https://raw.githubusercontent.com/Henryws/pacstall/master/pacstall -O /bin/pacstall
 fi
 if [[ $? -eq 1 ]] ; then
 echo "Distro not supported yet"
 exit 1
 fi
 echo "Henryws" >> /usr/share/pacstall/repo/pacstallrepo.txt
+sudo chmod a+x /bin/pacstall
 echo "Done"
 exit 0
