24c5da3b4039df82ed882fc15c2ab11a9123c449
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-27 14:35:00 -0500

    Update install.sh
    Former-commit-id: 7d953689afc6a8f9257bf2588508ee29de18e03a

--- packages/corectrl/install.sh
+++ packages/corectrl/install.sh
@@ -3,4 +3,4 @@ cd corectrl
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF ..
-sudo paco -lp corectrl "make -j$(nproc)"
+sudo porg -lp corectrl "make -j$(nproc)"
