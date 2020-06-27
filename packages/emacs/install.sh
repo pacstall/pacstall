1563460715276613b912c55474cc2b91edb580b9
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-27 14:01:14 -0500

    Update install.sh
    Former-commit-id: 8538b92ca46b70ab8ca2a3ca9ee818586868db33 [formerly 9bb25dbb3d7f93fcdf3edee7af138517d7f7f655] [formerly 4b8d6c985141abb8786ef431a45e3b9f409dc558]
    Former-commit-id: 374e1ec4fa977bb17b720bf69f6deb916124c21d
    Former-commit-id: 18f5fba8f0f344ab6c291739420f01b2a3c087ae

--- packages/emacs/install.sh
+++ packages/emacs/install.sh
@@ -3,10 +3,5 @@ wget -O emacs.tar.xz https://mirrors.ocf.berkeley.edu/gnu/emacs/emacs-26.3.tar.x
 tar -xf emacs.tar.xz
 cd emacs-26.3
 ./configure
-make
-if sudo checkinstall ; then
-    echo "checkinstall succeeded"
-else
-    echo "checkinstall failed... running make install"
-    sudo make install
-fi
+make -j$(nproc)
+sudo paco -lp emacs "make install"
