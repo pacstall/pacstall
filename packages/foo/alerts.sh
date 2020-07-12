83f73269da1434ce0e4e1d19a2d75083b8266b88
Henryws <hwengerstickel@gmail.com>
2020-07-12 17:26:50 -0500

    Create alerts.sh
    Former-commit-id: 8520f11cbbb4970770abd5abf018626f021ca740

---
+++ packages/foo/alerts.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "This is a development script that tests pacstalls dependency handling. If you just happened to type this in, you should probably not install unless you want to install emacs and figlet"
