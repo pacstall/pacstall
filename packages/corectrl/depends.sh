b102d6323a6eadbb501c40e5a92d89303eb53ac7
Henryws <hwengerstickel@gmail.com>
2020-07-24 14:38:50 -0500

    Update depends.sh
    Former-commit-id: ae26259a09c99c06875a8582e61db55ad50f2359
    Former-commit-id: f1f1b4c257c3e4ca247ab1728cbc98b60210c608

--- packages/corectrl/depends.sh
+++ packages/corectrl/depends.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-apt install extra-cmake-modules qt5-qmake qtdeclarative5-dev libqt5quick5 libqt5charts5-dev  qt5-default qttools5-dev libkf5auth-dev libkf5archive-dev libbotan-2-dev sysvinit-utils  libqt5dbus5  libqt5widgets5  libqt5network5 
+Ubuntu: extra-cmake-modules qt5-qmake qtdeclarative5-dev libqt5quick5 libqt5charts5-dev  qt5-default qttools5-dev libkf5auth-dev libkf5archive-dev libbotan-2-dev sysvinit-utils  libqt5dbus5  libqt5widgets5  libqt5network5 
