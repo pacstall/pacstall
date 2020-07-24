78c099c49bf9ce9739a8a92dbd1ccfbc736d610f
Henryws <hwengerstickel@gmail.com>
2020-07-24 14:35:29 -0500

    Update depends.sh
    Former-commit-id: ad077df2a4d1c09e633be1f2f5e565740ea97f11

--- packages/adapta-gtk-theme/depends.sh
+++ packages/adapta-gtk-theme/depends.sh
@@ -1,1 +1,2 @@
-#!/bin/bash
+Ubuntu: inkscape sassc autoconf automake libgdk-pixbuf2.0-dev libglib2.0-dev libxml2-utils pkgconfig
+Fedora: autoconf inkscape sassc automake gdk-pixbuf2 glib2-devel libxml2 pkgconfig sassc
