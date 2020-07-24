2c5646405e6271adc2ff57a1eb495d0e1987904b
Henryws <hwengerstickel@gmail.com>
2020-07-24 14:38:09 -0500

    Update depends.sh
    Former-commit-id: 5eacd26b27aec87a312c43857f5f8ff3bac64c91

--- packages/conky/depends.sh
+++ packages/conky/depends.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-equals=$(command -v apt)
-if [ $equals = /usr/bin/apt ] ; then
-    sudo apt-get install -y cmake libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev
-fi
-equals=$(command -v dnf)
-if [ $equals = /usr/bin/dnf ] ; then
-    sudo dnf install -y cmake imlib2 libcurl libX11 libXdamage glibc cairo libcurl libgcc lua-libs g++ ncurses-devel libx11-devel libXdamage-devel libXft-devel libXinerama-devel libcurl-devel lua-devel imlib2-devel
-fi
+Ubuntu: cmake libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev
+Fedora: cmake imlib2 libcurl libX11 libXdamage glibc cairo libcurl libgcc lua-libs g++ ncurses-devel libx11-devel libXdamage-devel libXft-devel libXinerama-devel libcurl-devel lua-devel imlib2-devel
