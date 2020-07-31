1548b8ade0e3e9106c8e5e5af51cf05bc729564d
Henryws <hwengerstickel@gmail.com>
2020-07-31 15:33:38 -0500

    Update depends.sh

--- packages/neofetch/depends.sh
+++ packages/neofetch/depends.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-equals=$(command -v apt)
-if [ $equals == /usr/bin/apt ] ; then
-    sudo apt install -y w3m-img libsixel termpix pixterm catimg jp2a caca-utils libcaca
-fi
-equals=$(command -v dnf)
-if [ $equals == /usr/bin/dnf ] ; then
-    sudo dnf install -y w3m-img caca-utils libcaca
-fi
+Ubuntu: w3m-img libsixel termpix pixterm catimg jp2a caca-utils libcaca
+Fedora: w3m-img caca-utils libcaca
