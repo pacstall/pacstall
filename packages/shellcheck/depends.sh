d7eaf2dcc8cf57d96605d021417c065305d9961d
Henryws <hwengerstickel@gmail.com>
2020-07-31 15:34:29 -0500

    Update depends.sh

--- packages/shellcheck/depends.sh
+++ packages/shellcheck/depends.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-equals=$(command -v apt)
-if [[ equals = /usr/bin/apt ]]
-then
-sudo apt install -y cabal-install
-fi
-equals=$(command -v dnf)
-if [[ $equals = /usr/bin/dnf ]]
-then
-sudo dnf install -y cabal-install
-fi
+Ubuntu: cabal-install
+Fedora: cabal-install
