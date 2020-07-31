f64cf76f2d31bba5d373c07147b11432a7ed9558
Henryws <hwengerstickel@gmail.com>
2020-07-31 15:23:14 -0500

    Update depends.sh

--- packages/dwm/depends.sh
+++ packages/dwm/depends.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-if [ command -v pacman -eq /usr/bin/pacman ]
-then
-sudo pacman -S dmenu freetype2 st libxinerama
-fi
-if [ command -v apt -eq /usr/bin/apt ]
-then
-sudo apt install -y dmenu freetype2 libxinerama
-fi
-
-
+Ubuntu: dmenu freetype2 libxinerama
