61a4a0f4f599c862ff1963e8fbc5eecfd4117312
Henryws <hwengerstickel@gmail.com>
2020-07-31 15:25:27 -0500

    Update depends.sh
    Former-commit-id: a38d2d3afd7f1a5431de671e43ac01c9dc7eed0b [formerly abbe88127c21d70247ba75412150440159c5e98c] [formerly e86bb4bcaad19e226e9dd5021616cc20463580a3]
    Former-commit-id: acdea5133d1726170b217c126bf4073654f95094
    Former-commit-id: 5387030e749b591230c8678c1e9f15b1c903324d

--- packages/gnome-tweaks/depends.sh
+++ packages/gnome-tweaks/depends.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-equals=$(command -v apt)
-if [ $equals == /usr/bin/apt ] ; then
-    sudo apt install -y python3-pip meson
-fi
-equals=$(command -v dnf)
-if [ $equals == /usr/bin/dnf ] ; then
-    sudo dnf install -y python3-pip ninja-build python27 python3-setuptools meson
-fi
-
+Ubuntu: python3-pip meson
+Fedora: python3-pip ninja-build python27 python3-setuptools meson
