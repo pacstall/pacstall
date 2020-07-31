caacbcca4194029b038eac1d348d7a8a6caa3e1d
Henryws <hwengerstickel@gmail.com>
2020-07-31 15:26:18 -0500

    Update depends.sh
    Former-commit-id: 406d11d7c1bf2820554916e5e1b325bd3ede5ef3
    Former-commit-id: 191eb264946243f8f9473d1a43bcdd3ce0b0699a

--- packages/kvantum/depends.sh
+++ packages/kvantum/depends.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-if [ command -v apt -eq /usr/bin/apt ]
-then
-sudo apt install -y qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev
-fi
-if [ command -v dnf -eq /usr/bin/dnf ]
-then
-sudo dnf install -y gcc-c++ libX11-devel libXext-devel qt5-qtx11extras-devel qt5-qtbase-devel qt5-qtsvg-devel qt5-qttools-devel kf5-kwindowsystem-devel qt5-qtbase-devel qt5-qtsvg-devel qt5-qtx11extras-devel kf5-kwindowsystem-devel
-fi
+Ubuntu: qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev
+Fedora: gcc-c++ libX11-devel libXext-devel qt5-qtx11extras-devel qt5-qtbase-devel qt5-qtsvg-devel qt5-qttools-devel kf5-kwindowsystem-devel qt5-qtbase-devel qt5-qtsvg-devel qt5-qtx11extras-devel kf5-kwindowsystem-devel
