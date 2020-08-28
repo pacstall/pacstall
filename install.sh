7abd675908caa829f17cd424c15974962e458006
Henryws <hwengerstickel@gmail.com>
2020-08-28 10:12:50 -0500

    Update install.sh
    Former-commit-id: 1892cf63faceae4be3ba6469ff9bd64d0545d712 [formerly 33e94501213eb4335b96294a252702c655b1b55c] [formerly 6c3c7c690cbeed9b8f871167df4e70aa66ff1732]
    Former-commit-id: f3c2f00ed3b1b5fdcbb2afd6ca3a6378af5a8724
    Former-commit-id: ec36f5d4374bfb5585b5dfadf9fb675241a0d4e0

--- install.sh
+++ install.sh
@@ -7,4 +7,20 @@ echo "|------------------------|"
 
 # Colors
 
-if [[ command -v curl ]]
+banner
+echo "checking for curl and wget"
+
+if [[ $(command -v curl) != "/usr/bin/curl" ]] ; then
+  echo "You seem to not have curl installed"
+  exit 1
+fi
+
+if [[ $(command -v wget) != "/usr/bin/wget" ]] ; then
+  echo "You seem to not have wget installed"
+  exit 1
+fi
+
+echo "making directories"
+mkdir -p /usr/share/pacstall/repo
+touch /usr/share/pacstall/repo/pacstallrepo.txt
+echo "Henryws/pacstall-programs" > /usr/share/pacstall/repo/pacstallrepo.txt
