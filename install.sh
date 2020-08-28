6868b3f2602a48135bde477a18f936c75e6635a3
Henryws <hwengerstickel@gmail.com>
2020-08-28 10:31:55 -0500

    Update install.sh
    Former-commit-id: 13a2febfee79ed82e41eb9339bc118e6619010c8

--- install.sh
+++ install.sh
@@ -8,29 +8,36 @@ echo " "
 }
 
 # Colors
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
 
 banner
 
 echo "this will do:
-* Check for curl and wget
-* Install Pacstall
+* Check for ${BLUE}curl${NC} and ${BLUE}wget${NC}
+* Install ${BLUE}Pacstall${NC}
   -- Create necessary directories
-  -- Wget into /bin/pacstall"
+  -- ${BLUE}Wget${NC} into ${PURPLE}/bin/pacstall${NC}"
 
-echo "checking for curl and wget"
+echo "checking for ${BLUE}curl${NC} and ${BLUE}wget${NC}"
 
 if [[ $(command -v curl) != "/usr/bin/curl" ]] ; then
-  echo "You seem to not have curl installed"
+  echo "You seem to not have ${BLUE}curl${NC} installed"
   exit 1
 fi
 
 if [[ $(command -v wget) != "/usr/bin/wget" ]] ; then
-  echo "You seem to not have wget installed"
+  echo "You seem to not have ${BLUE}wget${NC} installed"
   exit 1
 fi
 
 if [[ $(command -v porg) != "/usr/bin/porg" ]] ; then
-  echo "You seem to not have porg installed"
+  echo "You seem to not have ${BLUE}porg${NC} installed"
   exit 1
 fi
 
@@ -40,6 +47,6 @@ touch /usr/share/pacstall/repo/pacstallrepo.txt
 echo "Henryws/pacstall-programs" > /usr/share/pacstall/repo/pacstallrepo.txt
 touch /var/log/pacstall_installed
 touch /var/cache/pacstall/
-echo "pulling pacstall from https://raw.githubusercontent.com/Henryws/pacstall/$BRANCH/pacstall"
+echo "pulling ${BLUE}pacstall${NC} from ${RED}https://raw.githubusercontent.com/Henryws/pacstall/$BRANCH/pacstall${NC}"
 sudo wget -O /bin/pacstall https://raw.githubusercontent.com/Henryws/pacstall/$BRANCH/pacstall
 sudo chmod +x /bin/pacstall
