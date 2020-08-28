dc2af97587245715c8d023ffbddbdb4b155593b5
Henryws <hwengerstickel@gmail.com>
2020-08-28 10:18:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+BRANCH="1.0.4-Celeste"
 banner() {
 echo "|------------------------|"
 echo "|---Pacstall installer---|"
 echo "|------------------------|"
+echo " "
 }
 
 # Colors
@@ -20,7 +22,16 @@ if [[ $(command -v wget) != "/usr/bin/wget" ]] ; then
   exit 1
 fi
 
+if [[ $(command -v porg) != "/usr/bin/porg" ]] ; then
+  echo "You seem to not have porg installed"
+  exit 1
+fi
+
 echo "making directories"
 mkdir -p /usr/share/pacstall/repo
 touch /usr/share/pacstall/repo/pacstallrepo.txt
 echo "Henryws/pacstall-programs" > /usr/share/pacstall/repo/pacstallrepo.txt
+touch /var/log/pacstall_installed
+touch /var/cache/pacstall/
+echo "pulling pacstall from https://raw.githubusercontent.com/Henryws/pacstall/$BRANCH/pacstall"
+wget
