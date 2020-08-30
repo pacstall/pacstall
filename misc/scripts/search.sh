a3dd62f542f4e636d2e499241f79b1aa4846e2d8
Henryws <hwengerstickel@gmail.com>
2020-08-30 14:15:35 -0500

    Update search.sh
    Former-commit-id: 2f96600d0c176b161816c52f0bf97a37c2f619f4 [formerly 2621653a4cc82118bedc2b5d60a595c37356cce7] [formerly c2a80b0a7c14990bd0f1abfe38468d8c3d0149a3]
    Former-commit-id: 5635accf1006dbfa9e6f41e45cce4588c59f897e
    Former-commit-id: 5dd963f90e585b33265a6457ee6e96a6f916ffbb

--- misc/scripts/search.sh
+++ misc/scripts/search.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 SEARCH=$2
-     REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
-     wget -q --spider https://github.com/"$REPO"/tree/master/packages/$SEARCH/
-     if [ $? -eq 0 ]; then
-        read -p "${GREEN}$SEARCH${NC} is available in the ${PURPLE}$REPO${NC} repository. Do you want to view the pacscript? " answer
+REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
+wget -q --spider https://github.com/"$REPO"/tree/master/packages/$SEARCH/
+if [ $? -eq 0 ]; then
+	read -p "${GREEN}$SEARCH${NC} is available in the ${PURPLE}$REPO${NC} repository. Do you want to view the pacscript? " answer
 	if [[ $answer = y ]] ; then
-	curl https://raw.githubusercontent.com/$REPO/master/packages/$PACKAGE/PACSCRIPT
-	exit 0
+		curl https://raw.githubusercontent.com/$REPO/master/packages/$PACKAGE/PACSCRIPT
+		exit 0
 	else
 	exit 0
-        fi
-     else
-        echo "$SEARCH is not available. Add another repo or check your spelling"
+	fi
+else
+	echo "$SEARCH is not available. Add another repo or check your spelling"
 	exit 1
-     fi
+fi
