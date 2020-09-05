e230d77eed4c8bf03b2f88bdebdf89f7ea4abacc
Henryws <hwengerstickel@gmail.com>
2020-09-05 15:18:08 -0500

    Update install-local.sh
    Former-commit-id: da8e2ff9a7f4f551dc2ff718facb03578c08a3f0 [formerly c2632c300bc9bf0c3b3bb56ecc39d3d7c939a623] [formerly b1c5d2dd10a4d0ef3df5ddbe9bb1e2c9e2098e32]
    Former-commit-id: 8be807d84680e349c7769bf49d1644d5ae36a5f8
    Former-commit-id: 54dbf37018b6d528236cd4293392b5722723b271

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+checks() {
+if curl --output /dev/null --silent --head --fail "$url" ; then
+  echo "url exists"
+fi
+if apt-cache search $build_depends >/dev/null 2>&1 ; then
+  echo "build depends exists in repos"
+fi
+if apt-cache search $depends >/dev/null 2>&1 ; then
+  echo "dependencies exist in repos"
+fi
+}
 source PACSCRIPT
 echo "installing ${BLUE}$pkgname${NC} version: ${BLUE}$version${NC}"
-if [[ $build_depends
+echo "running checks"
+checks
+echo "${BLUE}$pkgname${NC} requires $(echo $build_depends) to install"
+echo "do you want to remove them after installing ${BLUE}$pkgname${NC} [y/n] "
+read -r REMOVE_DEPENDS
+dpkg-query -l $breaks >/dev/null 2>&1
+if [[ $? -eq 1 ]] ; then
+  echo "! ${RED}$pkgname${NC} breaks $breaks"
+  exit 1
+fi
