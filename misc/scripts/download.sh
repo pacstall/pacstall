7e3d4ba976ff20ed9e03fdac403ef822c52b8976
Henryws <hwengerstickel@gmail.com>
2020-09-05 12:30:09 -0500

    Update download.sh
    Former-commit-id: 43dca076315fe4f0e8a39d7fe47a93d645cf18b7
    Former-commit-id: d50117b3cd85a848c4d2c3262107057c1dee338f

--- misc/scripts/download.sh
+++ misc/scripts/download.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
+progressfilt ()
+{
+    local flag=false c count cr=$'\r' nl=$'\n'
+    while IFS='' read -d '' -rn 1 c
+    do
+        if $flag
+        then
+            printf '%s' "$c"
+        else
+            if [[ $c != $cr && $c != $nl ]]
+            then
+                count=0
+            else
+                ((count++))
+                if ((count > 1))
+                then
+                    flag=true
+                fi
+            fi
+        fi
+    done
+}
+
+
 PACKAGE=$2
 REPO=$3
+URL=https://raw.githubusercontent.com/$REPO/master/packages/$PACKAGE/PACSCRIPT
+if wget --spider $URL 2>/dev/null; then
+  echo "the file you want to download does not exists"
+  exit 1
+fi
+mkdir -p ~/.cache/pacstall/$PACKAGE
+cd ~/.cache/pacstall/$PACKAGE
+wget --progress=bar:force $URL 2>&1 | progressfilt
