5f601e28bcc5415a2766530fdd69a758f88db4d3
Henryws <hwengerstickel@gmail.com>
2020-09-05 14:40:45 -0500

    Update download.sh
    Former-commit-id: d61e8d429c14c52ed39195350ba246ad6f19a33f

--- misc/scripts/download.sh
+++ misc/scripts/download.sh
@@ -21,7 +21,6 @@ progressfilt ()
         fi
     done
 }
-
 download() {
 mkdir -p ~/.cache/pacstall/
 cd ~/.cache/pacstall/
@@ -29,9 +28,6 @@ mkdir -p $PACKAGE
 cd $PACKAGE
 wget --progress=bar:force $URL 2>&1 | progressfilt
 }
-
-PACKAGE=$1
-REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
 URL="https://raw.githubusercontent.com/$REPO/1.0.4-Celeste/packages/$PACKAGE/PACSCRIPT"
 if curl --output /dev/null --silent --head --fail "$URL" ; then
   download
