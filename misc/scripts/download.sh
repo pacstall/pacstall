3040ebf288bda39530a6bf310aa8f217a79007a2
Henryws <hwengerstickel@gmail.com>
2020-09-05 14:37:01 -0500

    Update download.sh
    Former-commit-id: 37780430d4c5d18ee834d7168ddb9cce39aebbb7

--- misc/scripts/download.sh
+++ misc/scripts/download.sh
@@ -22,14 +22,20 @@ progressfilt ()
     done
 }
 
+download() {
+mkdir -p ~/.cache/pacstall/
+cd ~/.cache/pacstall/
+mkdir -p $PACKAGE
+cd $PACKAGE
+wget --progress=bar:force $URL 2>&1 | progressfilt
+}
 
-PACKAGE=$2
-REPO=$3
-URL=https://raw.githubusercontent.com/$REPO/master/packages/$PACKAGE/PACSCRIPT
-if wget --spider $URL 2>/dev/null; then
+PACKAGE=$1
+REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
+URL="https://raw.githubusercontent.com/$REPO/1.0.4-Celeste/packages/$PACKAGE/PACSCRIPT"
+if curl --output /dev/null --silent --head --fail "$URL" ; then
+  download
+else
   echo "the file you want to download does not exists"
   exit 1
 fi
-mkdir -p ~/.cache/pacstall/$PACKAGE
-cd ~/.cache/pacstall/$PACKAGE
-wget --progress=bar:force $URL 2>&1 | progressfilt
