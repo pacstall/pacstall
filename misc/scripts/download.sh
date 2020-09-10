1b67aa1937b95b5cf031cd7e87f6009004b27549
Henryws <hwengerstickel@gmail.com>
2020-09-10 17:52:48 -0500

    Update download.sh

--- misc/scripts/download.sh
+++ misc/scripts/download.sh
@@ -30,17 +30,17 @@ cd $PACKAGE
 wget --progress=bar:force $URL 2>&1 | progressfilt
 }
 
-INPUT=$1
-echo $INPUT > /tmp/pacstalling
-if grep -q @ /tmp/pacstalling; then
-    PACKAGE=$(cut -d@ -f1 /tmp/pacstalling)
-    VERSION=$(cut -d@ -f2- /tmp/pacstalling)
-else
-    PACKAGE=$INPUT
-    VERSION=master
-fi
-REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
-URL="https://raw.githubusercontent.com/$REPO/1.0.4-Celeste/packages/$PACKAGE/$VERSION/PACSCRIPT"
+#INPUT=$1
+#echo $INPUT > /tmp/pacstalling
+#if grep -q @ /tmp/pacstalling; then
+#    PACKAGE=$(cut -d@ -f1 /tmp/pacstalling)
+#    VERSION=$(cut -d@ -f2- /tmp/pacstalling)
+#else
+#    PACKAGE=$INPUT
+#    VERSION=master
+#fi
+#REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
+#URL="https://raw.githubusercontent.com/$REPO/1.0.4-Celeste/packages/$PACKAGE/$VERSION/PACSCRIPT"
 if curl --output /dev/null --silent --head --fail "$URL" ; then
   download
 else
