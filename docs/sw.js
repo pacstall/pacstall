2da562be09af01d474fa82a0dd169b32dfb32a8a
Henryws <hwengerstickel@gmail.com>
2020-09-18 08:34:15 -0500

    Update sw.js
    Former-commit-id: e1ccfb5ba1e4739a9d839997ef0ada6ffb1d0062 [formerly d6dd81ced01fad8f20c5da9875e083ed038d252e] [formerly 47dbedf17ca1a4e91e8042f44010221e8af46e7d]
    Former-commit-id: df4ff8f6cf4321c1c77f488885fdb3fe4bbd971f
    Former-commit-id: 730dea78b2f93c7877098220841ff38bba941fc8

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 function registerServiceWorker() {
   // register sw script in supporting browsers
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
+    navigator.serviceWorker.register('sw.js', { scope: '/index.md' }).then(() => {
       console.log('Service Worker registered successfully.');
     }).catch(error => {
       console.log('Service Worker registration failed:', error);
