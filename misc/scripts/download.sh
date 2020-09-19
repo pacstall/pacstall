76d0be002ac432c0f79a66eb543aeb600ba535de
Henryws <hwengerstickel@gmail.com>
2020-09-19 23:13:20 +0000

    Update download.sh

--- misc/scripts/download.sh
+++ misc/scripts/download.sh
@@ -1,52 +1,47 @@
 #!/bin/bash
-progressfilt ()
-{
-    local flag=false c count cr=$'\r' nl=$'\n'
-    while IFS='' read -d '' -rn 1 c
-    do
-        if $flag
-        then
-            printf '%s' "$c"
-        else
-            if [[ $c != $cr && $c != $nl ]]
-            then
-                count=0
-            else
-                ((count++))
-                if ((count > 1))
-                then
-                    flag=true
-                fi
-            fi
-        fi
-    done
-}
-
-download() {
-mkdir -p $HOMEDIR/.cache/pacstall/
-cd $HOMEDIR/.cache/pacstall/
-mkdir -p $PACKAGE
-cd $PACKAGE
-wget --progress=bar:force $URL 2>&1 | progressfilt
-if [[ $INSTALLING -eq 1 ]] ; then
-    source /usr/share/pacstall/scripts/install-local.sh
+checks() {
+if curl --output /dev/null --silent --head --fail "$url" ; then
+  echo -e "url exists"
+fi
+if apt-cache search $build_depends >/dev/null 2>&1 ; then
+  echo -e "build depends exists in repos"
+fi
+if apt-cache search $depends >/dev/null 2>&1 ; then
+  echo -e "dependencies exist in repos"
 fi
 }
-source <(cat /tmp/pacvariables)
-#INPUT=$1
-#echo $INPUT > /tmp/pacstalling
-#if grep -q @ /tmp/pacstalling; then
-#    PACKAGE=$(cut -d@ -f1 /tmp/pacstalling)
-#    VERSION=$(cut -d@ -f2- /tmp/pacstalling)
-#else
-#    PACKAGE=$INPUT
-#    VERSION=master
-#fi
-#REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
-#URL="https://raw.githubusercontent.com/$REPO/1.0.4-Celeste/packages/$PACKAGE/$VERSION/PACSCRIPT"
-if curl --output /dev/null --silent --head --fail "$URL" ; then
-  download
-else
-  echo "! the file you want to download does not exist"
+source $PACKAGE.pacscript
+echo -e "running checks"
+checks
+if [[ $? -eq 1 ]] ; then
+  echo -e "! There was an error checking the script!"
   exit 1
 fi
+echo -e "${BLUE}$pkgname${NC} requires ${CYAN}$(echo -e $build_depends)${NC} to install"
+printf "do you want to remove them after installing ${BLUE}$pkgname${NC} [y/n] "
+read -r REMOVE_DEPENDS
+dpkg-query -l $breaks >/dev/null 2>&1
+if [[ $? -eq 0 ]] ; then
+  echo -e "! ${RED}$pkgname${NC} breaks $breaks"
+  exit 1
+fi
+sudo apt install $build_depends
+echo -e ":: Retrieving packages..."
+cd /tmp/
+if [[ $url = *.git ]] ; then
+  git clone $url
+else
+  wget --progress=bar:force $url
+  if [[ $url = *.zip ]] ; then
+    unzip $(echo ${url##*/})
+  else
+    tar -xf $(echo ${url##*/})
+  fi
+fi
+prepare
+build
+install
+if [[ $REMOVE_DEPENDS = y ]] ; then
+  sudo apt remove $build_depends
+fi
+echo -e ":: Done installing $name"
