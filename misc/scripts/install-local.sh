9461c9ec1584b0b6d0eca6aaca8819c1312b0155
Henryws <hwengerstickel@gmail.com>
2020-09-20 17:05:14 +0000

    Update install-local.sh
    Former-commit-id: 23c80cb4cf74ce4770378696af90c3fc521bae14 [formerly 16c04b1c06d71393c7c849c68174fb16402414dc] [formerly 9ac265ffacb9f8e1d6e67fd4f7abce04b181d953]
    Former-commit-id: bf2df3a5944f5e4425a4bd8619a68003e08bdc49
    Former-commit-id: 31713145045e2c0410a1e1950ba07787df0c434e

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 checks() {
 if curl --output /dev/null --silent --head --fail "$url" ; then
-  echo "url exists"
+  echo -e "url exists"
 fi
 if apt-cache search $build_depends >/dev/null 2>&1 ; then
-  echo "build depends exists in repos"
+  echo -e "build depends exists in repos"
 fi
 if apt-cache search $depends >/dev/null 2>&1 ; then
-  echo "dependencies exist in repos"
+  echo -e "dependencies exist in repos"
 fi
 }
 source $PACKAGE.pacscript
-echo ":: Installing ${BLUE}$pkgname${NC} version: ${BLUE}$version${NC}"
-echo "running checks"
+echo -e "running checks"
 checks
 if [[ $? -eq 1 ]] ; then
-  echo "! There was an error checking the script!"
+  echo -e "! There was an error checking the script!"
   exit 1
 fi
-echo "${BLUE}$pkgname${NC} requires $(echo $build_depends) to install"
-echo "do you want to remove them after installing ${BLUE}$pkgname${NC} [y/n] "
+echo -e "${BLUE}$pkgname${NC} requires ${CYAN}$(echo -e $build_depends)${NC} to install"
+printf "do you want to remove them after installing ${BLUE}$pkgname${NC} [y/n] "
 read -r REMOVE_DEPENDS
 dpkg-query -l $breaks >/dev/null 2>&1
-if [[ $? -eq 1 ]] ; then
-  echo "! ${RED}$pkgname${NC} breaks $breaks"
+if [[ $? -eq 0 ]] ; then
+  echo -e "! ${RED}$pkgname${NC} breaks $breaks"
   exit 1
 fi
-echo ":: Retrieving packages..."
+sudo apt install $build_depends
+echo -e ":: Retrieving packages..."
 cd /tmp/
 if [[ $url = *.git ]] ; then
   git clone $url
 else
   wget --progress=bar:force $url
   if [[ $url = *.zip ]] ; then
-    unzip -
+    unzip $(echo ${url##*/})
   else
-    tar -xf -
+    tar -xf $(echo ${url##*/})
   fi
 fi
 prepare
@@ -44,4 +44,4 @@ install
 if [[ $REMOVE_DEPENDS = y ]] ; then
   sudo apt remove $build_depends
 fi
-echo ":: Done installing $name"
+echo -e ":: Done installing $name"
