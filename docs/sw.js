0790e247ad05f976a5a17eb0a2eb2ad2771a8fc2
Henryws <hwengerstickel@gmail.com>
2020-09-22 07:51:52 -0500

    Update sw.js
    Former-commit-id: 3e1ace29b3acea2047e4e10af3614adbd71ac1b4
    Former-commit-id: f402383b30fb6f000ada425fc1852dfbb92a6186

--- docs/sw.js
+++ docs/sw.js
@@ -1,10 +1,15 @@
-function registerServiceWorker() {
-  // register sw script in supporting browsers
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/index.md' }).then(() => {
-      console.log('Service Worker registered successfully.');
-    }).catch(error => {
-      console.log('Service Worker registration failed:', error);
-    });
-  }
-}
+// sw.js
+self.addEventListener('install', e => {
+ e.waitUntil(
+   // after the service worker is installed,
+   // open a new cache
+   caches.open('my-pwa-cache').then(cache => {
+     // add all URLs of resources we want to cache
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.md',
+     ]);
+   })
+ );
+});
