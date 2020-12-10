43d42bc9a25965fc89d34c7b639ea9d1f51fd17a
Henry <hwengerstickel@protonmail.com>
2020-12-10 07:08:17 -0600

    install.sh has fancy_message now
    Former-commit-id: 5d88a692ab43edbba8f6a9038bbf0a6c2d58ef41 [formerly 6377869960ce0c7a9c68d4c741365486457a1f47] [formerly 79a19709a7fe1b967512e92e28d6785dad9f0dd4]
    Former-commit-id: 84397d396390cc342e1530f3e83542e667cf2f2b
    Former-commit-id: a88986a4bc623c4f73a67a94acc83418af933300

--- install.sh
+++ install.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+function fancy_message() {
+    # $1 = type , $2 = message
+    # Message types
+    # 0 - info
+    # 1 - warning
+    # 2 - error
+    if [ -z "${1}" ] || [ -z "${2}" ]; then
+      return
+    fi
+
+    local RED="\e[31m"
+    local GREEN="\e[32m"
+    local YELLOW="\e[33m"
+    local RESET="\e[0m"
+    local MESSAGE_TYPE=""
+    local MESSAGE=""
+    MESSAGE_TYPE="${1}"
+    MESSAGE="${2}"
+    
+    case ${MESSAGE_TYPE} in
+      info) echo -e "[${GREEN}+${RESET}] INFO: ${MESSAGE}";;
+      warn) echo -e "[${YELLOW}*${RESET}] WARNING: ${MESSAGE}";;
+      error) echo -e "[${RED}!${RESET}] ERROR: ${MESSAGE}";;
+      *) echo -e "[?] UNKNOWN: ${MESSAGE}";;
+    esac
+}
 banner() {
 echo -e "|------------------------|"
 echo -e "|---${GREEN}Pacstall Installer${NC}---|"
@@ -28,7 +54,7 @@ progressfilt() {
 }
 if ! command -v apt &> /dev/null
 then
-    echo "!! apt could not be found"
+    fancy_message error "apt could not be found"
     exit 1
 fi
 apt install -y sudo wget
@@ -52,7 +78,7 @@ do
   printf "\b${sp:i++%${#sp}:1}"
 done
 if [[ $? -eq 1 ]] ; then
-  echo "You seem to be offline"
+  fancy_message warn "You seem to be offline"
   exit 1
 fi
 echo ""
@@ -62,14 +88,14 @@ echo -e "this will do:
   -- Create necessary directories
   -- Pull ${BLUE}Pacstall${NC} with ${BLUE}wget${NC} from the ${YELLOW}Master${NC} branch into ${PURPLE}/bin/pacstall${NC}"
 echo ""
-echo -e "checking for ${BLUE}curl${NC} and ${BLUE}wget${NC}"
-echo "Installing curl" &
+fancy_message info "checking for ${BLUE}curl${NC} and ${BLUE}wget${NC}"
+fancy_message info "Installing curl" &
 sudo apt install -y curl 1>&1
 echo "Installing porg" &
 sudo apt install -y porg 1>&1
 unset PACSTALL_DIRECTORY
 export PACSTALL_DIRECTORY="/usr/share/pacstall"
-echo "making directories"
+fancy_message info "making directories"
 sudo mkdir -p $PACSTALL_DIRECTORY
 sudo mkdir -p $PACSTALL_DIRECTORY/scripts
 sudo mkdir -p $PACSTALL_DIRECTORY/repo
@@ -80,7 +106,7 @@ sudo echo "Henryws/pacstall-programs" > $PACSTALL_DIRECTORY/repo/pacstallrepo.tx
 sudo rm -rf /var/log/pacstall_installed
 sudo mkdir /var/log/pacstall_installed
 sudo rm -rf /var/cache/pacstall
-printf "Pulling scripts from GitHub "
+fancy_message info "Pulling scripts from GitHub "
 for i in {change-repo.sh,search.sh,download.sh,install-local.sh}; do 
 sudo wget -q -N https://raw.githubusercontent.com/Henryws/pacstall/master/misc/scripts/$i -P /usr/share/pacstall/scripts 2>/dev/null | progressfilt
 done &
@@ -94,8 +120,8 @@ do
   printf "\b${sp:i++%${#sp}:1}"
 done
 echo ""
-echo -e "pulling ${BLUE}pacstall${NC} from ${RED}https://raw.githubusercontent.com/Henryws/pacstall/master/pacstall${NC}"
+fancy_message info "pulling ${BLUE}pacstall${NC} from ${RED}https://raw.githubusercontent.com/Henryws/pacstall/master/pacstall${NC}"
 sudo wget --progress=bar:force -O /bin/pacstall https://raw.githubusercontent.com/Henryws/pacstall/master/pacstall 2>&1 | progressfilt
 sudo chmod +x /bin/pacstall
-echo -e "Installing ${BLUE}Manpage${NC}"
+fancy_message info "Installing ${BLUE}Manpage${NC}"
 wget --progress=bar:force -O /usr/share/man/man8/pacstall.8.gz https://raw.githubusercontent.com/Henryws/pacstall/master/misc/pacstall.8.gz 2>&1 | progressfilt
