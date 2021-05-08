39b25733ab23c6957427155e9c287db4bdc13755
Henry <hwengerstickel@protonmail.com>
2021-05-08 16:20:31 -0500

    fzf searches with dracula theme
    Former-commit-id: 8a39ba1d578ef4c42fef70655e6de80dee3cba78 [formerly 81cef2d47e4f79a3bba58111e092367803b1ea83] [formerly 976af6dba604cf874ac2ee0535044ef41ee45b99]
    Former-commit-id: 1a234f2be9756ba375edc6ca5b103ffc26919490
    Former-commit-id: 58512a47fcb774e7cf2fd4087c895ead00591497

--- misc/scripts/search.sh
+++ misc/scripts/search.sh
@@ -8,9 +8,15 @@ if [[ -z "$SEARCH" ]]; then
 fi
 
 REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
+FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color=dark
+--color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
+--color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
+'
 SELECTED=$(curl -s "$REPO"/packagelist | tr ' ' '\n' | fzf -q "$SEARCH")
 if ask "Do you want to view the pacscript?" N; then
     curl -s "$REPO"/packages/$SELECTED/$SELECTED.pacscript | less -R
     exit
 fi
+unset FZF_DEFAULT_OPTS
 exit
