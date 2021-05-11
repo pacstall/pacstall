5de22ceb16c50f0bb85c39a6ddd40ccfb9467d75
Henry <hwengerstickel@protonmail.com>
2021-05-11 16:23:01 -0500

    fixed -S
    Former-commit-id: 469baa94eefe7f80abf4625ae94a8e4d3db9add9
    Former-commit-id: b73b9560c4ec5af14796ad3d97f46ea2c7b57d5a

--- misc/scripts/search.sh
+++ misc/scripts/search.sh
@@ -13,7 +13,7 @@ FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
 --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
 --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
 '
-SELECTED=$(curl -s "$REPO"/packagelist | tr ' ' '\n' | fzf -q "$SEARCH")
+SELECTED=$(curl -s "$REPO"/packagelist | tr ' ' '\n' | fzf -q "$SEARCH" --preview="curl -s "$REPO"/packages/{}/{}.pacscript")
 if ask "Do you want to view the pacscript?" N; then
     curl -s "$REPO"/packages/$SELECTED/$SELECTED.pacscript | less -R
     exit
