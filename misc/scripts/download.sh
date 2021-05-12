639ea7e09738ba01f8cb6099765cfc826462e49a
Henry <hwengerstickel@protonmail.com>
2021-05-12 16:10:54 -0500

    Added license things

--- misc/scripts/download.sh
+++ misc/scripts/download.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+#    ____                  __        ____
+#   / __ \____ ___________/ /_____ _/ / /
+#  / /_/ / __ `/ ___/ ___/ __/ __ `/ / /
+# / ____/ /_/ / /__(__  ) /_/ /_/ / / /
+#/_/    \__,_/\___/____/\__/\__,_/_/_/
+#
+#    Copyright (C) DATE WHEN THE PROJECT 2020-2021
+#
+#       This file is part of Pacstall
+#
+#    Pacstall is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License
+#
+#    Pacstall is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Pacstall. If not, see <https://www.gnu.org/licenses/>.
 
 # This script downloads pacscripts from the interwebs
 
