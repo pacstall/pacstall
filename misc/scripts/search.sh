2dc16a6bed14847b568faa9d746b4ea0ad68f34f
Henry <hwengerstickel@protonmail.com>
2021-05-16 10:19:17 -0500

    Switched -S to grep
    Former-commit-id: e3cf5fc033b5d0e52bbc326c019c805de35f4889

--- misc/scripts/search.sh
+++ misc/scripts/search.sh
@@ -29,15 +29,5 @@ if [[ -z "$SEARCH" ]]; then
 fi
 
 REPO=$(cat /usr/share/pacstall/repo/pacstallrepo.txt)
-FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
---color=dark
---color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
---color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
-'
-SELECTED=$(curl -s "$REPO"/packagelist | tr ' ' '\n' | fzf -q "$SEARCH" --preview="curl -s "$REPO"/packages/{}/{}.pacscript")
-if ask "Do you want to install $SELECTED?" Y; then
-    sudo pacstall -I "$SELECTED"
-    exit
-fi
-unset FZF_DEFAULT_OPTS
+curl -s "$REPO"/packagelist | tr ' ' '\n' | \grep --color=always "$SEARCH"
 exit
