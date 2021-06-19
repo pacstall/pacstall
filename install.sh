d846dc8eb5ed497daa64cdc3fb27eadc13307dfa
Henry <hwengerstickel@protonmail.com>
2021-06-19 12:54:37 -0500

    Chan: chmod +x scripts

--- install.sh
+++ install.sh
@@ -118,6 +118,7 @@ sudo mkdir -p /var/db/pacstall
 fancy_message info "Pulling scripts from GitHub "
 for i in {change-repo.sh,search.sh,download.sh,install-local.sh,upgrade.sh,remove.sh,update.sh,query-info.sh}; do 
   sudo wget -q --show-progress -N https://raw.githubusercontent.com/pacstall/pacstall/master/misc/scripts/"$i" -P /usr/share/pacstall/scripts
+  sudo chmod +x /usr/share/pacstall/scripts/*
 done &
 PID=$!
 i=1
