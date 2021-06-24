8438436729568a717856a7e1ebd6e6b9b97a48e5
Sourajyoti Basak <wiz28@protonmail.com>
2021-06-24 17:16:18 +0530

    Feat: Cleanup (#85)
    * Fix: Add `function` keyword
    
    * Fix: Spacing
    
    * Fix: More spacing
    
    * Chan: Add exit codes
    
    * Chan: Cleanup `pacstall`
    
    * Fix: Typo
    
    Chan: `yonking` -> `yoinking`
    
    * Sug: Fix: Spacing
    
    * Chan: Fix spacing for good
    
    * Chan: Stuff
    
    * Chan: Move all major commands to scripts
    
    * Chan: chmod +x scripts
    
    * Fix: fix references
    
    * Fix: fix more references
    
    * Fix: Refactor references
    
    * Fix: Cleanup `change-repo.sh`
    
    * Chan: Cleanup `download.sh`
    
    Co-authored-by: Henry <hwengerstickel@protonmail.com>

---
+++ misc/scripts/remove.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+cd "$STOWDIR" || (sudo mkdir -p "$STOWDIR"; cd "$STOWDIR")
+
+# Run preliminary checks
+if [[ -z "$PACKAGE" ]]; then
+  fancy_message error "You failed to specify a package"
+  exit 1
+fi
+
+if [[ ! -d "$PACKAGE" ]]; then
+  fancy_message error "$PACKAGE is not installed or not properly symlinked"
+  exit 1
+fi
+
+# Removal starts from here
+source "$LOGDIR/$PACKAGE" > /dev/null 2>&1
+
+fancy_message info "Removing symlinks"
+sudo stow -D "$PACKAGE"
+
+fancy_message info "Removing package"
+sudo rm -rf "$PACKAGE"
+hash -r
+
+  if [[ $_removescript == "yes" ]] ; then
+    fancy_message info "Running post removal script"
+    REPO=$(cat "$STGDIR/repo/pacstallrepo.txt")
+  # Sources the output of /var/cache/pacstall/pkg/version/pkg.pacscript so we don't have to download scripts from the repo
+  source <(cat "/var/cache/pacstall/$PACKAGE/$(pacstall -V "$PACKAGE")/$PACKAGE.pacscript")
+    removescript
+fi
+
+if [ -n "$_dependencies" ]; then
+  fancy_message info "You may want to remove ${BLUE}$_dependencies${NC}"
+fi
+
+sudo rm -f "$LOGDIR/$PACKAGE"
+exit 0
