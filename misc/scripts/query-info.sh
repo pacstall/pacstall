e24bdf3c3a4a15a93ef845f7da9b3feb72629c4b
Sourajyoti Basak <wiz28@protonmail.com>
2021-06-24 17:16:18 +0530

    Feat: Cleanup (#85)
    * Fix: Add `function` keyword
    
    * Fix: Spacing
    
    * Fix: More spacing
    
    * Chan: Add exit codes
    
    * Chan: Cleanup `pacstall`
    
    * Fix: Typo
    
    Chan: `yonking` -> `yoinking`
    
    * Sug: Fix: Spacing
    
    * Chan: Fix spacing for good
    
    * Chan: Stuff
    
    * Chan: Move all major commands to scripts
    
    * Chan: chmod +x scripts
    
    * Fix: fix references
    
    * Fix: fix more references
    
    * Fix: Refactor references
    
    * Fix: Cleanup `change-repo.sh`
    
    * Chan: Cleanup `download.sh`
    
    Co-authored-by: Henry <hwengerstickel@protonmail.com>
    Former-commit-id: a077e26e064d5b76f0268afc5c1c53516b55f4c4 [formerly 574d9fd6843287d49ebbc0da5ee885bb19e91aac] [formerly 8438436729568a717856a7e1ebd6e6b9b97a48e5]
    Former-commit-id: e6085a4d8624cb621ae5fb28ee9cedc076d91cfb
    Former-commit-id: 416c78dc60b4e2811e757c73cd71ebfc74ba6cac

---
+++ misc/scripts/query-info.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ -z "$PACKAGE" ]]; then
+  fancy_message error "You failed to specify a package"
+  exit 1
+fi
+
+if [ ! -f "$LOGDIR/$PACKAGE" ]; then
+  fancy_message error "Package does not exist"
+  exit 1
+fi
+
+source "$LOGDIR/$PACKAGE"
+echo "name: $PACKAGE"
+echo "version: $_version"
+echo "size: $(du -sh "$STOWDIR"/"$PACKAGE" 2> /dev/null | awk '{print $1}')"
+echo "description: ""$_description"""
+echo "date installed: ""$_date"""
+
+if [[ -n $_maintainer ]]; then
+  echo "maintainer: ""$_maintainer"""
+fi
+if [[ -n $_pacdeps ]]; then
+  echo "pacstall dependencies: ""$_pacdeps"""
+fi
+if [[ -n $_dependencies ]]; then
+  echo "dependencies: ""$_dependencies"""
+fi
+if [[ -n $_build_dependencies ]]; then
+  echo "build dependencies: ""$_build_dependencies"""
+fi
+if [[ -n $_pacstall_depends ]]; then
+  echo "install type: installed as dependency"
+else
+  echo "install type: explicitly installed"
+fi
+exit 0
