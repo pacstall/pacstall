f2c0907a20a3efa340735898b6517896ac521203
Henry <hwengerstickel@protonmail.com>
2021-06-27 06:44:40 -0500

    Fix: query-info colors work

--- misc/scripts/query-info.sh
+++ misc/scripts/query-info.sh
@@ -11,27 +11,27 @@ if [ ! -f "$LOGDIR/$PACKAGE" ]; then
 fi
 
 source "$LOGDIR/$PACKAGE"
-echo "${BGreen}name${NORMAL}: $PACKAGE"
-echo "${BGreen}version${NORMAL}: $_version"
-echo "${BGreen}size${NORMAL}: $(du -sh "$STOWDIR"/"$PACKAGE" 2> /dev/null | awk '{print $1}')"
-echo "${BGreen}description${NORMAL}: ""$_description"""
-echo "${BGreen}date installed${NORMAL}: ""$_date"""
+echo -e "${BGreen}name${NORMAL}: $PACKAGE"
+echo -e "${BGreen}version${NORMAL}: $_version"
+echo -e "${BGreen}size${NORMAL}: $(du -sh "$STOWDIR"/"$PACKAGE" 2> /dev/null | awk '{print $1}')"
+echo -e "${BGreen}description${NORMAL}: ""$_description"""
+echo -e "${BGreen}date installed${NORMAL}: ""$_date"""
 
 if [[ -n $_maintainer ]]; then
-  echo "${BGreen}maintainer${NORMAL}: ""$_maintainer"""
+  echo -e "${BGreen}maintainer${NORMAL}: ""$_maintainer"""
 fi
 if [[ -n $_pacdeps ]]; then
-  echo "${BGreen}pacstall dependencies${NORMAL}: ""$_pacdeps"""
+  echo -e "${BGreen}pacstall dependencies${NORMAL}: ""$_pacdeps"""
 fi
 if [[ -n $_dependencies ]]; then
-  echo "${BGreen}dependencies${NORMAL}: ""$_dependencies"""
+  echo -e "${BGreen}dependencies${NORMAL}: ""$_dependencies"""
 fi
 if [[ -n $_build_dependencies ]]; then
-  echo "${BGreen}build dependencies${NORMAL}: ""$_build_dependencies"""
+  echo -e "${BGreen}build dependencies${NORMAL}: ""$_build_dependencies"""
 fi
 if [[ -n $_pacstall_depends ]]; then
-  echo "${BGreen}install type${NORMAL}: installed as dependency"
+  echo -e "${BGreen}install type${NORMAL}: installed as dependency"
 else
-  echo "${BGreen}install type${NORMAL}: explicitly installed"
+  echo -e "${BGreen}install type${NORMAL}: explicitly installed"
 fi
 exit 0
