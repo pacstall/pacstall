280adec6a48bd77cb2f08d1af65732bb09b2c5a6
David Brochero <davidfbg00@gmail.com>
2021-07-15 00:00:02 -0300

    Search script for multiple repos

--- misc/scripts/search.sh
+++ misc/scripts/search.sh
@@ -31,10 +31,60 @@ if [[ -z "$SEARCH" ]]; then
 	exit 3
 fi
 
-REPO=$(cat "$STGDIR"/repo/pacstallrepo.txt)
-# If ripgrep is installed, grep with ripgrep, else, do grep
-if command -v rg &> /dev/null; then
-  curl -s "$REPO"/packagelist | tr ' ' '\n' | rg --no-config --color=never -N "$SEARCH"
+PACKAGELIST=()
+URLLIST=()
+while IFS= read -r  URL; do
+  PARTIALLIST=($(curl -s "$URL"/packagelist))
+  PACKAGELIST+=(${PARTIALLIST[*]})
+  PACKAGELIST[-1]+=' '
+  URLLIST+=("${PARTIALLIST[@]/*/$URL}")
+done < "$STGDIR/repo/pacstallrepo.txt"
+
+IDXSEARCH=$(echo ${PACKAGELIST[*]} | tr ' ' '\n' | grep -n "$SEARCH" | cut -d : -f1| awk '{print $0"-1"}'|bc)
+LEN=($IDXSEARCH)
+LEN=${#LEN[@]}
+
+function parseRepo() {
+  local REPO="${1}"
+  SPLIT=($(echo $REPO | tr "/" "\n"))
+  
+  if command echo $REPO |grep "github" &> /dev/null;then
+    echo -e "\e]8;;https://github.com/${SPLIT[-3]}/${SPLIT[-2]}\a${SPLIT[-3]}\e]8;;\a"
+    
+  elif command echo $REPO |grep "gitlab" &> /dev/null;then
+    echo -e "\e]8;;https://gitlab.com/${SPLIT[-4]}/${SPLIT[-3]}\a${SPLIT[-4]}\e]8;;\a"
+  
+  else
+    echo "\e]8;;$REPO\a$REPO\e]8;;\a" 
+  fi
+}
+
+if [ $LEN -eq 0 ];then
+  fancy_message warn "There is no package with the name $IRed$SEARCH$NC"
+  
+elif [[ -z "$PACKAGE" ]];then
+  fancy_message info "There are $LEN package(s) with $IGreen$SEARCH$NC in their name:"
+  for IDX in $IDXSEARCH ; do
+    echo -e "$GREEN${PACKAGELIST[$IDX]}$CYAN @ $(parseRepo ${URLLIST[$IDX]}) $NC"
+  done
+  
 else
-  curl -s "$REPO"/packagelist | tr ' ' '\n' | \grep --color=never "$SEARCH"
+  if [ $LEN -eq 1 ];then
+    REPO=${URLLIST[0]}
+  else
+    echo "There are $LEN package(s) with $GREEN$SEARCH$NC in their name."
+    
+    if ask "Do you want to continue?" N;then
+      for IDX in $IDXSEARCH ; do
+        if ask "\e[1A\e[KDo you want to install $GREEN${PACKAGELIST[$IDX]}$NC from the repo $CYAN$(parseRepo ${URLLIST[$IDX]})$NC?" N;then
+          REPO=${URLLIST[$IDX]}
+          break
+        fi
+      done  
+    else
+      exit 1
+    fi
+  fi
 fi
+
+
