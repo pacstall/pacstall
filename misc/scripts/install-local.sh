767552352e2d1a18587f17d77823bf2d843cb3a1
NarutoXY <79555780+NarutoXY@users.noreply.github.com>
2021-07-17 21:01:50 +0530

    Prbbly fix hash

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -94,7 +94,7 @@ function loggingMeta() {
 function aria2 {
 fancy_message info "Downloading the package"
 if which aria2c; then
-sudo aria2c -c --download-result=hide "$url"
+sudo aria2c --download-result=hide "$url"
 else
 sudo wget -q --show-progress --progress=bar:force "$url" 2>&1
 fi
