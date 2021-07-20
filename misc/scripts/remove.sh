8f3054cdb53ddaa0b91b0a64ac8392a617e3ca6f
Sourajyoti Basak <wiz28@protonmail.com>
2021-07-20 17:41:51 +0530

    Style: Retab and add vim modeline (#134)
    * style(main): Retab and add vim settings
    
    * style(installer): Retab and add vim settings
    
    * style(completion): Retab and add vim settings
    
    * style(`add-repo`): Retab and add vim settings
    
    * style(`download`): Retab and add vim modeline
    
    * style(`install-local`): Retab and add vim modeline
    
    * style(`query-info`): Retab and add vim modeline
    
    * style(`remove`): Retab and add vim modeline
    
    * style(`search`): Retab and add vim modeline
    
    * style(`update`): Retab and add vim modeline
    
    * style(`upgrade`): Retab and add vim modeline
    
    * Fix super sneky commit @wizard-28
    
    * style(`completion`): Change function style
    
    Co-authored-by: The TwilightBlood <hwengerstickel@protonmail.com>
    Former-commit-id: 6951f2affd91dacc14f9ca2a39da1c0ec3e03bc8

--- misc/scripts/remove.sh
+++ misc/scripts/remove.sh
@@ -1,61 +1,60 @@
 #!/bin/bash
 
 function fn_exists() {
-  declare -F "$1" > /dev/null;
+	declare -F "$1" > /dev/null;
 }
 
 
 source "/var/cache/pacstall/$PACKAGE/$_version/$PACKAGE.pacscript"
 
 case "$url" in
-*.deb)
-sudo apt remove "$gives" 2>/dev/null
-if [[ $? -eq 0 ]]; then
-exit 0
-else
-fancy_message warn "Failed to remove the package"
-exit 1
-fi
-;;
+	*.deb)
+		if ! sudo apt remove "$gives" 2>/dev/null; then
+			fancy_message warn "Failed to remove the package"
+			exit 1
+		fi
+		exit 0
+	;;
 
-*)
-cd "$STOWDIR" || (sudo mkdir -p "$STOWDIR"; cd "$STOWDIR")
+	*)
+		cd "$STOWDIR" || (sudo mkdir -p "$STOWDIR"; cd "$STOWDIR")
 
-# Run preliminary checks
-if [[ -z "$PACKAGE" ]]; then
-  fancy_message error "You failed to specify a package"
-  exit 1
-fi
+		# Run preliminary checks
+		if [[ -z "$PACKAGE" ]]; then
+			fancy_message error "You failed to specify a package"
+			exit 1
+		fi
 
-if [[ ! -d "$PACKAGE" ]]; then
-  fancy_message error "$PACKAGE is not installed or not properly symlinked"
-  exit 1
-fi
+		if [[ ! -d "$PACKAGE" ]]; then
+			fancy_message error "$PACKAGE is not installed or not properly symlinked"
+			exit 1
+		fi
 
-# Removal starts from here
-source "$LOGDIR/$PACKAGE" > /dev/null 2>&1
+		# Removal starts from here
+		source "$LOGDIR/$PACKAGE" > /dev/null 2>&1
 
-fancy_message info "Removing symlinks"
-sudo stow --target="/" -D "$PACKAGE"
+		fancy_message info "Removing symlinks"
+		sudo stow --target="/" -D "$PACKAGE"
 
-fancy_message info "Removing package"
-sudo rm -rf "$PACKAGE"
-# Update PATH database
-hash -r
+		fancy_message info "Removing package"
+		sudo rm -rf "$PACKAGE"
+		# Update PATH database
+		hash -r
 
-if fn_exists removescript ; then
-    fancy_message info "Running post removal script"
-    REPO=$(cat "$STGDIR/repo/pacstallrepo.txt")
-    removescript
-fi
+		if fn_exists removescript ; then
+			fancy_message info "Running post removal script"
+			REPO=$(cat "$STGDIR/repo/pacstallrepo.txt")
+			removescript
+		fi
 
-if [ -n "$_dependencies" ]; then
-  fancy_message info "You may want to remove ${BLUE}$_dependencies${NC}"
-fi
+		if [ -n "$_dependencies" ]; then
+			fancy_message info "You may want to remove ${BLUE}$_dependencies${NC}"
+		fi
 
-sudo rm -f "$LOGDIR/$PACKAGE"
-exit 0
-;;
+		sudo rm -f "$LOGDIR/$PACKAGE"
+		exit 0
+	;;
 esac
 
 exit
+# vim:set ft=sh ts=4 sw=4 noet:
