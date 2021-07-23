d95186e6ccc9d73c0f716b0dbd0db2594ed8f4a9
D Brox <65723952+D-Brox@users.noreply.github.com>
2021-07-23 16:09:10 -0300

    Fix (scripts): Missing space in error log messages

--- misc/scripts/error_log.sh
+++ misc/scripts/error_log.sh
@@ -38,67 +38,67 @@ function error_log() {
 	
 	case "$code" in
 		1)
-			echo "[$(date) | $scope ] Error 1 - Unknown cause of failure." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 1 - Unknown cause of failure." >> "$LOGFILE"
 			return 0
 		;;
 		2)
-			echo "[$(date) | $scope ] Error 2 - Error in configuration file." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 2 - Error in configuration file." >> "$LOGFILE"
 			return 0
 		;;
 		3)
-			echo "[$(date) | $scope ] Error 3 - User specified an invalid option." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 3 - User specified an invalid option." >> "$LOGFILE"
 			return 0
 		;;
 		4)
-			echo "[$(date) | $scope ] Error 4 - Error in user-supplied function in pacscript." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 4 - Error in user-supplied function in pacscript." >> "$LOGFILE"
 			return 0
 		;;
 		5)
-			echo "[$(date) | $scope ] Error 5 - Failed to create a viable package." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 5 - Failed to create a viable package." >> "$LOGFILE"
 			return 0
 		;;
 		6)
-			echo "[$(date) | $scope ] Error 6 - A source or auxiliary file specified in the pacscript is missing." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 6 - A source or auxiliary file specified in the pacscript is missing." >> "$LOGFILE"
 			return 0
 		;;
 		7)
-			echo "[$(date) | $scope ] Error 7 - The STOWDIR is missing." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 7 - The STOWDIR is missing." >> "$LOGFILE"
 			return 0
 		;;
 		8)
-			echo "[$(date) | $scope ] Error 8 - Failed to install dependencies." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 8 - Failed to install dependencies." >> "$LOGFILE"
 			return 0
 		;;
 		9)
-			echo "[$(date) | $scope ] Error 9 - Failed to remove dependencies." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 9 - Failed to remove dependencies." >> "$LOGFILE"
 			return 0
 		;;
 		10)
-			echo "[$(date) | $scope ] Error 10 - User attempted to run pacstall as root." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 10 - User attempted to run pacstall as root." >> "$LOGFILE"
 			return 0
 		;;
 		11)
-			echo "[$(date) | $scope ] Error 11 - User lacks permissions to build or install to a given location." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 11 - User lacks permissions to build or install to a given location." >> "$LOGFILE"
 			return 0
 		;;
 		12)
-			echo "[$(date) | $scope ] Error 12 - Error parsing  pacscript." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 12 - Error parsing  pacscript." >> "$LOGFILE"
 			return 0
 		;;
 		13)
-			echo "[$(date) | $scope ] Error 13 - A package has already been built." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 13 - A package has already been built." >> "$LOGFILE"
 			return 0
 		;;
 		14)
-			echo "[$(date) | $scope ] Error 14 - The package failed to install." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 14 - The package failed to install." >> "$LOGFILE"
 			return 0
 		;;
 		15)
-			echo "[$(date) | $scope ] Error 15 - Programs necessary to run pacstall are missing." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 15 - Programs necessary to run pacstall are missing." >> "$LOGFILE"
 			return 0
 		;;
 		16)
-			echo "[$(date) | $scope ] Error 16 - Specified hash does not exist or failed to sign package." >> "$LOGFILE"
+			echo "[ $(date) | $scope ] Error 16 - Specified hash does not exist or failed to sign package." >> "$LOGFILE"
 			return 0
 		;;
 	esac
