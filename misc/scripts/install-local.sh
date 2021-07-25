c6733cb60877223c6681679b1920a9ead47b2418
Henry <hwengerstickel@protonmail.com>
2021-07-25 06:34:11 -0500

    Feat: fakeroot installs fakeroot if not found

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -320,14 +320,14 @@ export pkgdir="/usr/src/pacstall/$name"
 # fakeroot is weird but this method works
 # create tmp variable that is the output of what prepare function is (it prints out function)
 
-if command -v fakeroot > /dev/null; then
+if ! command -v fakeroot > /dev/null; then
+	sudo apt-get install fakeroot -y
+else
 	tmp_prepare=$(declare -f prepare)
 	# We run fakeroot, BUT, we don't actually pass any variables through to fakeroot. In other words, bash works with the tmp_prepare, instead of fakeroot
 	fakeroot -- bash -c "$tmp_prepare; prepare"
 	# Unset because it's a tmp variable
 	unset tmp_prepare
-else
-	prepare
 fi
 
 # Check if build function doesn't exist
@@ -337,12 +337,12 @@ if ! type -t build > /dev/null 2>&1; then
 	return 1
 fi
 
-if command -v fakeroot > /dev/null; then
+if ! command -v fakeroot > /dev/null; then
+	sudo apt-get install fakeroot -y
+else
 	tmp_build=$(declare -f build)
 	fakeroot -- bash -c "$tmp_build; build"
 	unset tmp_build
-else
-	build
 fi
 
 # Trap so that we can clean up (hopefully without messing up anything)
