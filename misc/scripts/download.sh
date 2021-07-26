83861ee81e0abd8f79d3b240656dfc2ee6e2fe5d
Sourajyoti Basak <wiz28@protonmail.com>
2021-07-26 20:39:46 +0530

    rem(`download`): Unnecessary creation of `PACKAGE` dir

--- misc/scripts/download.sh
+++ misc/scripts/download.sh
@@ -33,7 +33,6 @@ fi
 if curl --output /dev/null --silent --head --fail "$URL" ; then
 	if [[ "$type" = "install" ]]; then
 		mkdir -p "/tmp/pacstall/pacscripts/" && cd "/tmp/pacstall/pacscripts/"
-		mkdir -p "$PACKAGE" && cd "$PACKAGE"
 	fi
 
 	download "$URL" > /dev/null 2>&1
