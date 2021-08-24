d24cde17b3cd30cc53d59253c699be68a55b8b65
NarutoXY <79555780+NarutoXY@users.noreply.github.com>
2021-08-24 18:55:24 +0530

    Add: fancy_message
    Former-commit-id: 015bf8ec9a03242bc61e16fb879bf10a0fa18ea4

---
+++ src/pacstall.py
@@ -0,0 +1,17 @@
+COLORS = {
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "YELLOW": "\033[93m",
+    "PURPLE": "\033[95m",
+    "ENDC": "\033[0m",
+}
+
+def fancy_message(message_type, message):
+    if message_type == "info":
+        print(COLORS["GREEN"], "[+] INFO:", COLORS["ENDC"], message) 
+    elif message_type == "warn":
+        print(COLORS["YELLOW"], "[*] WARN:", COLORS["ENDC"], message) 
+    elif message_type == "error":
+        print(COLORS["RED"], "[!] ERROR:", COLORS["ENDC"], message)
+    else:
+        print(COLORS["ENDC"], "UNKNOWN: ", COLORS["ENDC"], message)
