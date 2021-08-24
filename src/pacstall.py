a422bb277efe19457afea6bced93c63adecd12a7
NarutoXY <79555780+NarutoXY@users.noreply.github.com>
2021-08-24 19:43:54 +0530

    Feat: ask

--- src/pacstall.py
+++ src/pacstall.py
@@ -1,3 +1,6 @@
+import os
+
+# ANSI CODE ESCAPE VALUES
 COLORS = {
     "GREEN": "\033[92m",
     "RED": "\033[91m",
@@ -6,12 +9,36 @@ COLORS = {
     "ENDC": "\033[0m",
 }
 
+# FANCY MESSAGE
 def fancy_message(message_type: str, message: str):
+    # INFO
     if message_type == "info":
-        print(COLORS["GREEN"], "[+] INFO:", COLORS["ENDC"], message) 
+        print(COLORS["GREEN"], "[+] INFO:", COLORS["ENDC"], message)
+    # WARN
     elif message_type == "warn":
-        print(COLORS["YELLOW"], "[*] WARN:", COLORS["ENDC"], message) 
+        print(COLORS["YELLOW"], "[*] WARN:", COLORS["ENDC"], message)
+    # ERROR
     elif message_type == "error":
         print(COLORS["RED"], "[!] ERROR:", COLORS["ENDC"], message)
+    # UNKNOWN
     else:
         print(COLORS["ENDC"], "[?] UNKNOWN: ", COLORS["ENDC"], message)
+
+def ask(ques: str, qtype=None):
+    default = qtype
+    if default == "Y":
+        print(ques, "[" + COLORS["GREEN"] + "Y" + COLORS["ENDC"] + "/n]")
+    elif default == "N":
+        print(ques, "[y/" + COLORS["RED"] + "N]" + COLORS["ENDC"])
+    else:
+        print(ques, "[y/n]")
+    a = { 'Y': 1, 'N': 0 }
+    while(True):
+        if os.environ.get('PACSTALL_PROMPTS_DISABLED') == True:
+            return default
+
+        answer = input().upper()
+        if answer in ["Y", "N"]:
+            return a[answer]
+        elif default is not None:
+            return a[qtype]
