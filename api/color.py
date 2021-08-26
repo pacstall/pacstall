cccea2c7e65274f71b0fbedb19077a40537ead32
Sourajyoti Basak <wiz28@protonmail.com>
2021-08-27 00:11:01 +0530

    docs(api): Add docs and boilerplate

--- api/color.py
+++ api/color.py
@@ -1,4 +1,43 @@
+#!/bin/env python3
+
+#     ____                  __        ____
+#    / __ \____ ___________/ /_____ _/ / /
+#   / /_/ / __ `/ ___/ ___/ __/ __ `/ / /
+#  / ____/ /_/ / /__(__  ) /_/ /_/ / / /
+# /_/    \__,_/\___/____/\__/\__,_/_/_/
+#
+# Copyright (C) 2020-2021
+#
+# This file is part of Pacstall
+#
+# Pacstall is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License
+#
+# Pacstall is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pacstall. If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Pacstall's color API
+
+Classes
+-------
+Style: Modify text styles
+Foreground: Modify foreground colors
+Background: Modify background colors
+"""
+
+
 class Style:
+    """
+    Modify text styles
+    """
+
     BOLD = "\033[1m"
     DIM = "\033[2m"
     ITALIC = "\033[3m"
@@ -9,6 +48,18 @@ class Style:
 
 
 class Foreground:
+    """
+    Modify foreground colors
+
+    Sections
+    --------
+    normal(no prefix): Normal colors
+    bold("B" prefix): Bold colors
+    underline("U" prefix): Underlined colors
+    high intensity("I" prefix): High intensity colors
+    bold high intensity("BI" prefix): Bold high intensity colors
+    """
+
     BLACK = "\033[0;30m"
     RED = "\033[0;31m"
     GREEN = "\033[0;32m"
@@ -60,6 +111,15 @@ class Foreground:
 
 
 class Background:
+    """
+    Modify background colors
+
+    Sections
+    --------
+    normal(no prefix): Normal colors
+    high intensity("I" prefix): High intensity colors
+    """
+
     Black = "\033[40m"
     Red = "\033[41m"
     Green = "\033[42m"
