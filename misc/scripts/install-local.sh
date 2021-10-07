21ffefa6786a7ae91879d5e695b84beb731ce16b
The TwilightBlood <hwengerstickel@protonmail.com>
2021-10-07 09:43:38 -0500

    feat(scripts): Run `postinst` & `removescript` for `-deb` (#281)

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -402,6 +402,9 @@ case "$url" in
 		fi
 		if sudo apt install -y -f ./"${url##*/}" 2>/dev/null; then
 			log
+			if type -t postinst > /dev/null 2>&1; then
+				postinst
+			fi
 
 			fancy_message info "Storing pacscript"
 			sudo mkdir -p /var/cache/pacstall/"$PACKAGE"/"$version"
