739524f6d02ed0d6406ece02fc1c735820b154f6
Harshwardhan Mehrotra <harshwardhanmehrotra@gmail.com>
2021-10-12 16:05:15 +0530

    fix(installer): Add `iputils-ping` dependency (#293)
    Former-commit-id: a6c1fa3a7fa379084ab60fb35786d305848a8a0e

--- install.sh
+++ install.sh
@@ -144,7 +144,7 @@ if [[ "$answer" -eq 1 ]]; then
     apt-get install -qq -y axel
 fi
 
-apt-get install -qq -y curl wget stow build-essential unzip tree bc git
+apt-get install -qq -y curl wget stow build-essential unzip tree bc git iputils-ping
 
 LOGDIR="/var/log/pacstall/metadata"
 STGDIR="/usr/share/pacstall"
