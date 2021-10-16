fbd5e86982aaa3efd8371d4bfca342b45e66a634
D Brox <65723952+D-Brox@users.noreply.github.com>
2021-10-16 18:57:52 -0300

    Fix(install): Wrong comparison
    I also moved the ppa thingy before all apt installs

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -293,6 +293,12 @@ fi
 # Trap Crtl+C just before the point cleanup is first needed
 trap "trap_ctrlc" 2
 
+if [[ -n "$ppa" ]]; then
+	for i in "${ppa[@]}"; do
+		# Add ppa, but ppa bad I guess
+		sudo add-apt-repository ppa:"$i"
+	done
+fi
 
 if [[ -n "$pacdeps" ]]; then
 	for i in "${pacdeps[@]}"; do
@@ -343,13 +349,6 @@ if [[ -n $replace ]]; then
 	fi
 fi
 
-if [[ -n "$ppa" ]]; then
-	for i in "${ppa[@]}"; do
-		# Add ppa, but ppa bad I guess
-		sudo add-apt-repository ppa:"$i"
-	done
-fi
-
 # Get all uninstalled build depends
 for build_dep in $build_depends; do
 	if dpkg-query -W -f='${Status}' "${build_dep}" 2> /dev/null | grep "^install ok installed" > /dev/null 2>&1; then
@@ -363,7 +362,7 @@ build_depends=$(echo "$build_depends" | tr -s ' ' | awk '{gsub(/^ +| +$/,"")} {p
 if [[ -n "$build_depends" ]]; then
 	fancy_message info "${BLUE}$name${NC} requires ${CYAN}$(echo -e "$build_depends")${NC} to install"
 	ask "Do you want to remove them after installing ${BLUE}$name${NC}" N
-	if [[ $answer -eq 1 ]]; then
+	if [[ $answer -eq 0 ]]; then
 		NOBUILDDEP=0
 	else
 		NOBUILDDEP=1
