b2732685cc131688abd02d3f100aa76aab1f4115
The TwilightBlood <hwengerstickel@protonmail.com>
2021-10-25 12:37:54 -0500

    Change workflow to skip network check

--- .github/workflows/workflow.sh
+++ .github/workflows/workflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt install -y sudo bc wget iputils-ping network-manager
-export PACSTALL_NO_INTERNET=1
+export PACSTALL_SKIP_NETWORK_CHECK=1
 echo "N" | sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
 useradd -m -d /home/pacstall pacstall
 usermod -a -G sudo pacstall
