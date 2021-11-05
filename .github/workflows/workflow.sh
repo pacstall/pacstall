2b0db5bc07566755a6e3799f18faa600337f7e65
The TwilightBlood <hwengerstickel@protonmail.com>
2021-11-05 08:29:27 -0500

    Re-add apt to workflow

--- .github/workflows/workflow.sh
+++ .github/workflows/workflow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+apt update
+apt install curl sudo git wget bc
 export PACSTALL_SKIP_NETWORK_CHECK=1
 echo "N" | sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
 useradd -m -d /home/pacstall pacstall
