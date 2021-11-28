1b24d103ad5d493aa38e6fff97b95cd3b52bf3da
Sourajyoti Basak <wiz28@protonmail.com>
2021-11-28 20:19:01 +0530

    style(download): Add missing error comment

--- pacstall/cmds/download.py
+++ pacstall/cmds/download.py
@@ -64,7 +64,7 @@ def execute(url: str, filepath: str = None) -> int:
                     file.write(data.content)
             except IOError:
                 fancy("error", "Could not write downloaded contents to file")
-                return 2
+                return 2  # --> Writing data failed
             return 0  # --> No problems occurred while downloading
 
     except (
