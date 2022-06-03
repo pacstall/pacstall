de56d787340e7def035eea58d53f4bf445ea5e77
Plasma <hwengerstickel@protonmail.com>
2022-06-03 11:52:20 -0500

    feat(update): display useful links and update status (#588)
    Co-authored-by: D Brox <65723952+D-Brox@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- misc/scripts/update.sh
+++ misc/scripts/update.sh
@@ -36,8 +36,14 @@ sudo chown "$PACSTALL_USER" -R /tmp/pacstall
 sudo mkdir -p /usr/share/bash-completion/completions
 
 STGDIR="/usr/share/pacstall"
+tabs -4
 
 tty_settings=$(stty -g)
+old_version=($(pacstall -V))
+old_info=($(cat $STGDIR/repo/update))
+
+old_username="${old_info[0]}"
+old_branch="${old_info[1]}"
 
 for i in {error_log.sh,add-repo.sh,search.sh,download.sh,install-local.sh,upgrade.sh,remove.sh,update.sh,query-info.sh}; do
 	sudo wget -q -N https://raw.githubusercontent.com/"$USERNAME"/pacstall/"$BRANCH"/misc/scripts/"$i" -P "$STGDIR/scripts" &
@@ -56,24 +62,52 @@ wait && stty "$tty_settings"
 sudo chmod +x /bin/pacstall
 sudo chmod +x /usr/share/pacstall/scripts/*
 
+echo "$USERNAME $BRANCH" | sudo tee "$STGDIR/repo/update" > /dev/null
+
+new_info=($(cat $STGDIR/repo/update))
+new_version=($(pacstall -V))
+
+new_username="${new_info[0]}"
+new_branch="${new_info[1]}"
+
 # Bling Bling update ascii
-echo '
+if [[ ${new_branch} != "master" ]]; then
+	echo '
+                                     ∩~-~∩
+ _____               _        _ _   ξ ･×･ ξ
+|  __ \             | |      | | |  ξ　~　ξ
+| |__) |_ _  ___ ___| |_ __ _| | |  ξ　　 ξ
+|  ___/ _` |/ __/ __| __/ _` | | |  ξ　　 “～～～〇
+| |  | (_| | (__\__ \ || (_| | | |  ξ　　 　　　 ξ
+|_|   \__,_|\___|___/\__\__,_|_|_|  ξ_ξ ξ_ξ ξ_ξξ_ξ
+'
+else
+	echo '
     ____                  __        ____
    / __ \____ ___________/ /_____ _/ / /
   / /_/ / __ `/ ___/ ___/ __/ __ `/ / /
  / ____/ /_/ / /__(__  ) /_/ /_/ / / /
 /_/    \__,_/\___/____/\__/\__,_/_/_/
 '
-if [[ $USERNAME == "pacstall" ]] && [[ $BRANCH == "master" ]]; then
-	echo -e "[${BGreen}+${NC}] INFO: You are at version $(pacstall -V)"
-	echo -e "[${BYellow}*${NC}] WARNING: Be sure to check our GitHub release page to make sure you have no incompatible code: https://github.com/pacstall/pacstall/releases"
-else
-	echo -e "[${BYellow}*${NC}] WARNING: You are using a ${RED}development${NC} version of $(pacstall -V)"
-	echo -e "[${BYellow}*${NC}] WARNING: There may be bugs in the code."
-	echo -e "[${BYellow}*${NC}] WARNING: Please report them to the Pacstall team through \e]8;;https://github.com/pacstall/pacstall/issues\aGitHub\e]8;;\a or \e]8;;https://discord.com/invite/yzrjXJV6K8\aDiscord\e]8;;\a"
+fi
+
+echo -e "[${BGreen}+${NC}] INFO: Updated from ${BGreen}${old_version}${NC} (${BGreen}${old_username} ${old_branch}${NC}) -> ${BGreen}${new_version}${NC} (${BGreen}${new_username} ${new_branch}${NC})"
+
+if [[ ${new_branch} != "master" ]]; then
+	echo -e "[${BGreen}+${NC}] INFO: You have updated to a development branch."
+	echo -e "[${BYellow}*${NC}] WARN: Please remember that bugs may arise, and that this branch may not be as stable as master."
 
 fi
-echo "$USERNAME $BRANCH" | sudo tee "$STGDIR/repo/update" > /dev/null
+
+if [[ ${new_username} == "pacstall" ]]; then
+	echo -e "Useful links:"
+	echo -e "\t${BYellow}Website${NC}: ${BOLD}https://pacstall.dev${NORMAL}"
+	echo -e "\t${BPurple}Packages${NC}: ${BOLD}https://pacstall.dev/packages${NORMAL}"
+	echo -e "\t${BCyan}GitHub${NC}: ${BOLD}https://github.com/pacstall${NORMAL}"
+	echo -e "\t${BRed}Report Bugs${NC}: ${BOLD}https://github.com/${new_username}/pacstall/issues${NORMAL}"
+	echo -e "\t${BBlue}Discord${NC}: ${BOLD}https://discord.gg/yzrjXJV6K8${NORMAL}"
+	echo -e "\t${BGreen}Matrix${NC}: ${BOLD}https://matrix.to/#/#pacstall:matrix.org${NORMAL}"
+fi
 exit 0
 
 # vim:set ft=sh ts=4 sw=4 noet:
