02296aeee7737903566368cfe625e9b9b7abc50a
The TwilightBlood <hwengerstickel@protonmail.com>
2022-08-25 16:08:31 -0500

    fix(select_options): export select_options

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -679,7 +679,7 @@ export srcdir="$PWD"
 sudo chown -R "$PACSTALL_USER":"$PACSTALL_USER" . 2> /dev/null
 
 export pkgdir="/usr/src/pacstall/$name"
-export -f ask fancy_message
+export -f ask fancy_message select_options
 
 # Trap so that we can clean up (hopefully without messing up anything)
 trap cleanup ERR
