0477257715449d4f54beb65fcec1e63a3d1701d0
Plasma <hwengerstickel@protonmail.com>
2022-10-04 16:43:11 -0500

    Show what function is being run

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -738,6 +738,7 @@ done
 if [[ -n "${pac_functions[*]}" ]]; then
 	fancy_message info "Running functions"
 	for function in "${pac_functions[@]}"; do
+		fancy_message sub "Running $function"
 		bash -ceuo pipefail "source $pacfile
 		$function" || {
 			error_log 5 "$function $PACKAGE"
