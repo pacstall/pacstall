77668dd108e3fce1915a9b43e40440fcb5f09567
Plasma <hwengerstickel@protonmail.com>
2022-10-09 12:26:06 -0500

    cat pacscript into post{inst,rm}

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -398,16 +398,7 @@ export homedir="$(get_homedir)"
 
 hash -r' | sudo tee "$STOWDIR/$name/DEBIAN/$deb_post_file" > /dev/null
             {
-                echo -e "export name=\"${name}\""
-                if [[ -n ${pkgname} ]]; then
-                    echo -e "export pkgname=\"${pkgname}\""
-                fi
-                echo -e "export pkgdir=\"${pkgdir}\""
-                if [[ -n ${gives} ]]; then
-                    echo -e "export gives=\"${gives}\""
-                fi
-                echo -e "export version=\"${version}\""
-                echo -e "export maintainer=\"${maintainer:-Pacstall <pacstall@pm.me>}\""
+				cat "$PACFILE"
                 echo -e "$(declare -f "$i")\n$i"
             } | sudo tee -a "$STOWDIR/$name/DEBIAN/$deb_post_file" > /dev/null
         fi
