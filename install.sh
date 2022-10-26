27d02918553df1c4806aa7812c2a0d5b0ede5f09
Plasma <hwengerstickel@protonmail.com>
2022-10-26 10:37:34 -0500

    fix(install.sh): install `curl` (#694)

--- install.sh
+++ install.sh
@@ -86,7 +86,7 @@ if ! command -v apt &> /dev/null; then
     fancy_message error "apt could not be found"
     exit 1
 fi
-apt-get install -y -qq sudo wget iputils-ping
+apt-get install -y -qq sudo wget curl iputils-ping
 
 echo -e "|------------------------|"
 echo -e "|---${GREEN}Pacstall Installer${NC}---|"
