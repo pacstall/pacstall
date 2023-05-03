0c9b92667e6beb035ede836be3ef01adb86a3c12
Plasma <hwengerstickel@protonmail.com>
2023-05-03 16:04:12 -0500

    feat(pacscript): add `NCPU` variable bound to `nproc` (#896)

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -167,7 +167,7 @@ function compare_remote_version() (
 function get_incompatible_releases() {
     # example for this function is "ubuntu:jammy"
     local distro_name="$(lsb_release -si 2> /dev/null)"
-	distro_name="${distro_name,,}"
+    distro_name="${distro_name,,}"
     if [[ "$(lsb_release -ds 2> /dev/null | tail -c 4)" == "sid" ]]; then
         local distro_version_name="sid"
         local distro_version_number="sid"
@@ -510,6 +510,12 @@ function get_homedir() {
 }
 export homedir="$(get_homedir)"
 
+if [[ -n $PACSTALL_BUILD_CORES ]]; then
+	declare -gr NCPU="${PACSTALL_BUILD_CORES:-1}"
+else
+	declare -gr NCPU="$(nproc)"
+fi
+
 hash -r' | sudo tee "$STOWDIR/$name/DEBIAN/$deb_post_file" > /dev/null
             {
                 cat "${pacfile}"
@@ -604,17 +610,22 @@ Pin-Priority: -1" | sudo tee /etc/apt/preferences.d/"${name}-pin" > /dev/null
     fi
 }
 
+# NCPU is the core count
 if [[ -n $PACSTALL_BUILD_CORES ]]; then
     if [[ $PACSTALL_BUILD_CORES =~ ^[0-9]+$ ]]; then
         function nproc() {
             echo "${PACSTALL_BUILD_CORES:-1}"
         }
+        declare -rg NCPU="${PACSTALL_BUILD_CORES:-1}"
     else
         fancy_message error "${UCyan}PACSTALL_BUILD_CORES${NC} is not an integer. Falling back to 1"
         function nproc() {
             echo "1"
         }
+        declare -rg NCPU="1"
     fi
+else
+    declare -rg NCPU="$(nproc)"
 fi
 
 ask "(${BPurple}$PACKAGE${NC}) Do you want to view/edit the pacscript" N
