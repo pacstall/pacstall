22cb6500d49230f37ee91150591f22f9f5bb6420
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-06-12 14:51:42 -0500

    Testing

--- misc/scripts/dep-tree.sh
+++ misc/scripts/dep-tree.sh
@@ -28,70 +28,89 @@
 export LOGDIR="/var/log/pacstall/metadata"
 
 function dep_tree.has_deps() {
-	local le_pkg="${1:?No pkg given to dep_tree.has_deps}"
-	if [[ -n $(dpkg-query '--showformat=${Depends}\n' --show "${le_pkg}") ]]; then
-		return 0
-	else
-		return 1
-	fi
+    local le_pkg="${1:?No pkg given to dep_tree.has_deps}"
+    if [[ -n $(dpkg-query '--showformat=${Depends}\n' --show "${le_pkg}") ]]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function dep_tree.load_traits() {
-	local pkg
-	local -n out_arr
-	pkg="${1:?No pkg given to dep_tree.load_traits}"
-	out_arr="${2:?No arr given to dep_tree.load_traits}"
-	unset -n _pacstall_depends _pacdeps 2> /dev/null
-	source "${LOGDIR}/${pkg}"
+    local pkg
+    local -n out_arr
+    local -n pacdeps
+    pkg="${1:?No pkg given to dep_tree.load_traits}"
+    out_arr="${2:?No arr given to dep_tree.load_traits}"
+    pacdeps="${3:?No pacdeps arr given to dep_tree.load_traits}"
+    unset -n _pacstall_depends _pacdeps 2> /dev/null
+    source "${LOGDIR}/${pkg}"
 
-	if [[ -n ${_pacstall_depends} ]]; then
-		out_arr['upgrade']=false
-	else
-		out_arr['upgrade']=true
-	fi
-	if [[ -n ${_pacdeps[*]} ]]; then
-		out_arr['pacdeps']=true
-	else
-		out_arr['pacdeps']=false
-	fi
-	if dep_tree.has_deps "${_gives:-${_name}}"; then
-		out_arr['depends']=true
-	else
-		out_arr['depends']=false
-	fi
+    if [[ -n ${_pacstall_depends} ]]; then
+        out_arr['upgrade']=false
+    else
+        out_arr['upgrade']=true
+    fi
+    if [[ -n ${_pacdeps[*]} ]]; then
+        out_arr['pacdeps']=true
+        pacdeps+=("${_pacdeps[@]}")
+    else
+        out_arr['pacdeps']=false
+    fi
+    if dep_tree.has_deps "${_gives:-${_name}}"; then
+        out_arr['depends']=true
+    else
+        out_arr['depends']=false
+    fi
 }
 
 function dep_tree.sort_traits_into_array() {
-	local pkg="${1:?No pkg given to dep_tree.sort_traits_into_array}"
-	local -n trait c_one c_two c_three
-	local trait="${2:?No trait array given to dep_tree.sort_traits_into_array}"
-	c_one="${3:?No c_one array given to dep_tree.sort_traits_into_array}"
-	c_two="${4:?No c_two array given to dep_tree.sort_traits_into_array}"
-	c_three="${5:?No c_three array given to dep_tree.sort_traits_into_array}"
+    local pkg="${1:?No pkg given to dep_tree.sort_traits_into_array}"
+    local -n trait c_one c_two c_three
+    local trait="${2:?No trait array given to dep_tree.sort_traits_into_array}"
+    c_one="${3:?No c_one array given to dep_tree.sort_traits_into_array}"
+    c_two="${4:?No c_two array given to dep_tree.sort_traits_into_array}"
+    c_three="${5:?No c_three array given to dep_tree.sort_traits_into_array}"
 
-	if [[ ${trait['upgrade']} == 'false' ]]; then
-		return 0
-	fi
+    if [[ ${trait['upgrade']} == 'false' ]]; then
+        return 0
+    fi
 
-	if [[ ${trait['pacdeps']} == 'false' && ${trait['depends']} == 'false' ]]; then
-		c_one+=("${pkg}")
-	elif [[ ${trait['pacdeps']} == 'true' && ${trait['depends']} == 'false' ]]; then
-		c_two+=("${pkg}")
-	else
-		c_three+=("${pkg}")
-	fi
+    if [[ ${trait['pacdeps']} == 'false' && ${trait['depends']} == 'false' ]]; then
+        c_one+=("${pkg}")
+    elif [[ ${trait['pacdeps']} == 'true' && ${trait['depends']} == 'false' ]]; then
+        c_two+=("${pkg}")
+    else
+        c_three+=("${pkg}")
+    fi
 }
 
 function dep_tree.loop_traits() {
-	local -n merged_array="${1:?No array given to dep_tree.loop_traits}"
-	shift
-	local class_one=() class_two=() class_three=() i
-	for i in "${@}"; do
-		local -A arr=()
-		dep_tree.load_traits "$i" arr
-		dep_tree.sort_traits_into_array "$i" arr class_one class_two class_three
-	done
-	merged_array=("${class_one[@]}" "${class_two[@]}" "${class_three[@]}")
+    local -n merged_array="${1:?No array given to dep_tree.loop_traits}"
+    shift
+    local class_one=() class_two=() class_three=() pacdeps_arr=() i
+    for i in "${@}"; do
+        echo -n "Loading traits of ${i}"
+        local -A arr=()
+        dep_tree.load_traits "$i" arr pacdeps_arr
+        dep_tree.sort_traits_into_array "$i" arr class_one class_two class_three
+        echo -ne "\033[2K\rParsed ${i}"
+    done
+    echo -ne "\033[2K\r"
+    merged_array=("${class_one[@]}" "${class_two[@]}" "${class_three[@]}")
+    # Remove all pacdeps logged
+    if ((${#pacdeps_arr[@]} > 0)); then
+        local idx=0
+        for pacdep in "${pacdeps_arr[@]}"; do
+            for j in "${merged_array[@]}"; do
+                if [[ ${j} == "${pacdep}" ]]; then
+                    unset 'merged_array[idx]'
+                fi
+                ((idx++))
+            done
+        done
+		merged_array=("${merged_array[@]}")
+    fi
 }
 
 dep_tree.loop_traits update_order $(pacstall -L)
