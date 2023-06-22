fca69838bcc7122a5e06b7b8c3b40772ab894462
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-06-22 10:52:35 -0500

    Nvm Oren

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -22,7 +22,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Pacstall. If not, see <https://www.gnu.org/licenses/>.
 
-set -x
 function cleanup() {
     if [[ -n $KEEP ]]; then
         rm -rf "/tmp/pacstall-keep/$name"
