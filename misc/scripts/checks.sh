0cf0554a7b9859237d5a919fda6824af930bfb17
Elsie <elsie19@pm.me>
2023-08-05 14:49:04 -0500

    No regex, just glob

--- misc/scripts/checks.sh
+++ misc/scripts/checks.sh
@@ -332,7 +332,7 @@ function lint_incompatible() {
         done
         idx=0
         for incompat in "${incompatible[@]}"; do
-            if [[ ! $incompat =~ ^[a-zA-Z0-9*]+:[a-zA-Z0-9*]+$ ]] || [[ $incompat == "*:*" ]]; then
+            if [[ ! $incompat == *:* ]] || [[ $incompat == "*:*" ]]; then
                 fancy_message error "'incompatible' index '${idx}' is improperly formatted"
                 ret=1
             fi
