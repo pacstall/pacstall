f766a894b233710f47c40f2320e35fe1d7f9f9eb
David Brochero <65723952+D-Brox@users.noreply.github.com>
2023-11-03 23:15:28 -0300

    fix(install-local): install pacdeps from same repo on upgrade  (#1000)
    * fix(install-local): Install pacdeps from same repo on upgrade
    
    * fix(install-local): Remove the old bug comment

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -816,7 +816,7 @@ if [[ -n $pacdeps ]]; then
             pacstall_pacdep_status="$(compare_remote_version "$i")"
             if [[ $pacstall_pacdep_status == "update" ]]; then
                 fancy_message info "Found newer version for $i pacdep"
-                if ! pacstall "$cmd" "$i"; then
+                if ! pacstall "$cmd" "${i}${repo}"; then
                     fancy_message error "Failed to install dependency"
                     error_log 8 "install $PACKAGE"
                     cleanup
@@ -826,7 +826,6 @@ if [[ -n $pacdeps ]]; then
                 fancy_message info "The pacstall dependency ${i} is already installed and at latest version"
 
             fi
-            # BUG: Pacstall installs pacdeps from main repo. In the RS version we should get the latest version of the pacdep from all repos and use that.
         elif fancy_message info "Installing $i" && ! pacstall "$cmd" "${i}${repo}"; then
             fancy_message error "Failed to install dependency"
             error_log 8 "install $PACKAGE"
