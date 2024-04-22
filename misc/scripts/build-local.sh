4f07105d8ec897caac1f36beefeec17ed00af429
oklopfer <104327997+oklopfer@users.noreply.github.com>
2024-04-22 15:18:08 -0400

    Update build-local.sh

--- misc/scripts/build-local.sh
+++ misc/scripts/build-local.sh
@@ -520,18 +520,28 @@ function makedeb() {
         done
     fi
 
-    deblog "Installed-Size" "$(sudo du -s --apparent-size --exclude=DEBIAN -- "$STOWDIR/$pkgname" | cut -d$'\t' -f1)"
-    install_size="$(sudo du -sbh --exclude=DEBIAN -- "$STOWDIR/$pkgname" | cut -d$'\t' -f1 | numfmt --from=iec --to=si --format="%3.2f" | awk '{
-        if (match($0, /[A-Za-z]+$/)) {
-            num = sprintf("%.3g", $1);
-            unit = substr($0, RSTART, RLENGTH);
-            if (unit == "K") unit = "k";
-            printf "%s %sB\n", num, unit;
-        } else {
-            num = sprintf("%3.2f", $1);
-            printf "%s B\n", num;
-        }
-    }')"
+	local estsize
+	estsize="$(sudo du -s --apparent-size --exclude=DEBIAN -- "$STOWDIR/$pkgname" | cut -d$'\t' -f1)"
+	deblog "Installed-Size" "${estsize}"
+    if ((estsize<10)); then
+        install_size="$((estsize * 1024)) B"
+    else
+        install_size="$(sudo du -sbh --exclude=DEBIAN -- "$STOWDIR/$pkgname" \
+            | cut -d$'\t' -f1 \
+            | numfmt --from=iec --to=si --format="%3.2f" \
+            | awk '{
+                if (match($0, /[A-Za-z]+$/)) {
+                    num = sprintf("%.3g", $1);
+                    unit = substr($0, RSTART, RLENGTH);
+                    if (unit == "K") unit = "k";
+                    printf "%s %sB\n", num, unit;
+                } else {
+                    num = sprintf("%3.2f", $1);
+                    printf "%s B\n", num;
+                }
+            }'
+        )"
+    fi
     export install_size
 
     generate_changelog | sudo tee -a "$STOWDIR/$pkgname/DEBIAN/changelog" > /dev/null
