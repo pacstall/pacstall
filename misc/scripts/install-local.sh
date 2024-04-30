05e8331dcb2d21c13371459da51002025b349b1f
oklopfer <104327997+oklopfer@users.noreply.github.com>
2024-04-29 21:36:35 -0400

    Update install-local.sh

--- misc/scripts/install-local.sh
+++ misc/scripts/install-local.sh
@@ -206,7 +206,7 @@ if [[ -n $pacdeps ]]; then
         [[ $KEEP ]] && cmd+="K"
         [[ $DISABLE_PROMPTS == "yes" ]] && cmd+="P"
         [[ $NOCHECK ]] && cmd+="Nc"
-        [[ $PACSTALL_VERBOSE ]] || cmd+="Qd"
+        [[ $PACSTALL_VERBOSE ]] || cmd+="q"
 
         if pacstall -S "${i}@${REPO}" &> /dev/null; then
             repo="@${REPO}"
