2106d641838575241055f3534f8a5ec0275f0c21
oklopfer <104327997+oklopfer@users.noreply.github.com>
2024-05-22 16:12:11 -0400

    feat(add-repo.sh): allow `-Rr`/`--remove-repo` (#1118)
    * feat(add-repo.sh): allow `-Rr`/`--remove-repo`
    
    * prompt
    
    * normalize
    
    * localize
    
    ---------
    
    Co-authored-by: ook37 <oren+12345@taumoda.com>

--- misc/scripts/add-repo.sh
+++ misc/scripts/add-repo.sh
@@ -22,47 +22,86 @@
 # You should have received a copy of the GNU General Public License
 # along with Pacstall. If not, see <https://www.gnu.org/licenses/>.
 
+parse_repo() {
+    local ADDR
+    IFS=':' read -ra ADDR <<< "$1"
+    PROV="${ADDR[0]}"
+    USER=$(echo "${ADDR[1]}" | cut -d'/' -f1)
+    HEAD=$(echo "${ADDR[1]}" | cut -d'/' -f2 | cut -d'#' -f1)
+    if [[ ${ADDR[1]} =~ "#" ]]; then
+        BRANCH="$(echo "${ADDR[1]}" | cut -d'#' -f2)"
+    else
+        BRANCH="master"
+        fancy_message warn "Assuming that git branch is ${GREEN}master${NC}"
+    fi
+}
+
 REPO="${2%/}"
 
-if [[ $REPO == *"github.com"* ]]; then
-    REPO="${REPO/'github.com'/'raw.githubusercontent.com'}"
-    if [[ $REPO != *"/tree/"* ]]; then
-        REPO="$REPO/master"
-        fancy_message warn "Assuming that git branch is ${GREEN}master${NC}"
-    else
-        REPO="${URL/'/tree/'/'/'}"
-    fi
-elif [[ $REPO == *"gitlab.com"* ]]; then
-    if [[ $REPO != *"/tree/"* ]]; then
-        REPO="$REPO/-/raw/master"
-        fancy_message warn "Assuming that git branch is ${GREEN}master${NC}"
-    else
-        REPO="${REPO/"/tree/"/"/raw/"}"
-    fi
-elif [[ -d $REPO ]] > /dev/null; then
-    if [[ $REPO != *"file://"* ]]; then
-        REPO="file://$(readlink -f "$REPO")"
-    fi
-else
-    fancy_message warn "The repo link must be the root to the raw files"
-    fancy_message warn "Make sure the repo contains a package list"
+case ${REPO} in
+    *"github.com"*)
+        REPO="${REPO/'github.com'/'raw.githubusercontent.com'}"
+        if [[ $REPO != *"/tree/"* ]]; then
+            REPO="$REPO/master"
+            fancy_message warn "Assuming that git branch is ${GREEN}master${NC}"
+        else
+            REPO="${REPO/'/tree/'/'/'}"
+        fi
+        ;;
+    *"gitlab.com"*)
+        if [[ $REPO != *"/tree/"* ]]; then
+            REPO="$REPO/-/raw/master"
+            fancy_message warn "Assuming that git branch is ${GREEN}master${NC}"
+        else
+            REPO="${REPO/"/tree/"/"/raw/"}"
+        fi
+        ;;
+    *"github:"*)
+        parse_repo "${REPO}"
+        REPO="https://raw.${PROV}usercontent.com/${USER}/${HEAD}/${BRANCH}"
+        ;;
+    *"gitlab:"*)
+        parse_repo "${REPO}"
+        REPO="https://${PROV}.com/${USER}/${HEAD}/-/raw/${BRANCH}"
+        ;;
+    *)
+        [[ ${REPO} == "local:"* ]] && REPO="file://${REPO/local:/}"
+        if [[ -d $REPO ]] > /dev/null; then
+            if [[ $REPO != *"file://"* ]]; then
+                REPO="file://$(readlink -f "$REPO")"
+            fi
+        fi
+        ;;
+esac
 
-    ask "Do you want to add \"$REPO\" to the repo list?" N
-    if ((answer == 0)); then
-        exit 3
-    fi
-fi
-
-if ! curl --head --location -s --fail -- "$REPO/packagelist" > /dev/null; then
-    fancy_message warn "If the URL is a private repo, edit ${CYAN}\e]8;;file://$SCRIPTDIR/repo/pacstallrepo\a$SCRIPTDIR/repo/pacstallrepo\e]8;;\a${NC}"
-    fancy_message error "packagelist file not found"
-    exit 3
-fi
-REPOLIST=()
-while IFS= read -r REPOURL; do
-    REPOLIST+=("${REPOURL}")
-done < "$SCRIPTDIR/repo/pacstallrepo"
-REPOLIST+=("$REPO")
+case ${REPOCMD} in
+    add)
+        ask "Do you want to add ${CYAN}${REPO}${NC} to the repo list?" Y
+        if ((answer == 0)); then
+            exit 3
+        fi
+        if ! curl --head --location -s --fail -- "$REPO/packagelist" > /dev/null; then
+            fancy_message warn "If the URL is a private repo, edit ${CYAN}\e]8;;file://$SCRIPTDIR/repo/pacstallrepo\a$SCRIPTDIR/repo/pacstallrepo\e]8;;\a${NC}"
+            fancy_message error "packagelist file not found"
+            exit 3
+        fi
+        REPOLIST=()
+        while IFS= read -r REPOURL; do
+            REPOLIST+=("${REPOURL}")
+        done < "$SCRIPTDIR/repo/pacstallrepo"
+        REPOLIST+=("$REPO")
+        ;;
+    remove)
+        ask "Do you want to remove ${CYAN}${REPO}${NC} from the repo list?" Y
+        if ((answer == 0)); then
+            exit 3
+        fi
+        REPOLIST=()
+        while IFS= read -r REPOURL; do
+            [[ ${REPOURL} != "$REPO" ]] && REPOLIST+=("${REPOURL}")
+        done < "$SCRIPTDIR/repo/pacstallrepo"
+        ;;
+esac
 
 printf "%s\n" "${REPOLIST[@]}" | sort -u | sudo tee "$SCRIPTDIR/repo/pacstallrepo" > /dev/null
 fancy_message info "The repo list has been updated"
