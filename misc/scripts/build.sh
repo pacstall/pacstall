3e3611f8ab38f221118d166c5ab83b76ef13653d
oklopfer <104327997+oklopfer@users.noreply.github.com>
2024-09-01 17:32:39 -0400

    sanity check

--- misc/scripts/build.sh
+++ misc/scripts/build.sh
@@ -61,19 +61,27 @@ function check_gen_dep() {
     local onlyname="${1}" onlyarch="${2}" onlyreal="${3}" onlywhere="${4}"
     if [[ ${onlyname} == *":${onlyarch}" ]]; then
         if [[ -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname%:*})?architecture(${onlyarch})" -F "%p")" ]]; then
-            if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname%:*}$)?architecture(${onlyarch})" -F "%p")" ]]; then
-                fancy_message sub $"%b [required]" "${CYAN}${onlyreal}${NC} ${RED}✗${NC}"
-                echo "${onlyreal}" >> "${onlywhere}"
-                return 0
+            if [[ -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname%:*})?architecture(all)" -F "%p")" ]]; then
+                if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname%:*}$)?architecture(${onlyarch})" -F "%p")" ]]; then
+                    if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname%:*}$)?architecture(all)" -F "%p")" ]]; then
+                        fancy_message sub $"%b [required]" "${CYAN}${onlyreal}${NC} ${RED}✗${NC}"
+                        echo "${onlyreal}" >> "${onlywhere}"
+                        return 0
+                    fi
+                fi
             fi
         fi
     else
         if [[ -z "$(apt-cache search --no-generate --names-only "^${onlyname}\$" 2> /dev/null || apt-cache search --names-only "^${onlyname}\$")" ]]; then
             if [[ -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname})?architecture(${onlyarch})" -F "%p")" ]]; then
-                if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname}$)?architecture(${onlyarch})" -F "%p")" ]]; then
-                    fancy_message sub $"%b [required]" "${CYAN}${onlyreal}${NC} ${RED}✗${NC}"
-                    echo "${onlyreal}" >> "${onlywhere}"
-                    return 0
+                if [[ -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname})?architecture(all)" -F "%p")" ]]; then
+                    if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname}$)?architecture(${onlyarch})" -F "%p")" ]]; then
+                        if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname}$)?architecture(all)" -F "%p")" ]]; then
+                            fancy_message sub $"%b [required]" "${CYAN}${onlyreal}${NC} ${RED}✗${NC}"
+                            echo "${onlyreal}" >> "${onlywhere}"
+                            return 0
+                        fi
+                    fi
                 fi
             fi
         fi
