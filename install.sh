a33548f0b2ca280dd8e1124a388520f411adcd47
Elsie <hwengerstickel@protonmail.com>
2024-09-01 16:23:06 -0400

    Update install.sh
    Co-authored-by: David Brochero <65723952+D-Brox@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ echo -e "${PACYELLOW}┌──────────────────�
 echo
 eval "$(apt-config shell Cache Dir::Cache)"
 eval "$(apt-config shell List Dir::State::Lists)"
-if [[ -z "$(find -H "${Cache}/${List}" -maxdepth 0 -mtime -7)" ]]; then
+if [[ -z "$(find -H "/${Cache}/${List}" -maxdepth 0 -mtime -7)" ]]; then
     fancy_message info "Updating"
     case "${GITHUB_ACTIONS}" in
         true) apt-get update -qq ;;
