0902e476ecd13310155461051948ce3ccbd176a4
ook37 <oren+12345@taumoda.com>
2024-09-12 15:18:56 -0400

    fix(build.sh): revert to nested ifs

--- misc/scripts/build.sh
+++ misc/scripts/build.sh
@@ -59,19 +59,32 @@ function clean_builddir() {
 function check_gen_dep() {
     { ignore_stack=false; set -o pipefail; trap stacktrace ERR RETURN; }
     local onlyname="${1}" onlyarch="${2}" onlyreal="${3}" onlywhere="${4}"
-    if [[ ${onlyname} == *":${onlyarch}" \
-        && -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname%:*})?architecture(${onlyarch})" -F "%p")" \
-        && -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname%:*})?architecture(all)" -F "%p")" \
-        && -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname%:*}$)?architecture(${onlyarch})" -F "%p")" \
-        && -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname%:*}$)?architecture(all)" -F "%p")" ]] \
-        || [[ -z "$(apt-cache search --no-generate --names-only "^${onlyname}\$" 2> /dev/null || apt-cache search --names-only "^${onlyname}\$")" \
-            && -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname})?architecture(${onlyarch})" -F "%p")" \
-            && -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname})?architecture(all)" -F "%p")" \
-            && -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname}$)?architecture(${onlyarch})" -F "%p")" \
-            && -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname}$)?architecture(all)" -F "%p")" ]]; then
-        fancy_message sub $"%b [required]" "${CYAN}${onlyreal}${NC} ${RED}✗${NC}"
-        echo "${onlyreal}" >> "${onlywhere}"
-        return 0
+    if [[ ${onlyname} == *":${onlyarch}" ]]; then
+        if [[ -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname%:*})?architecture(${onlyarch})" -F "%p")" ]]; then
+            if [[ -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname%:*})?architecture(all)" -F "%p")" ]]; then
+                if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname%:*}$)?architecture(${onlyarch})" -F "%p")" ]]; then
+                    if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname%:*}$)?architecture(all)" -F "%p")" ]]; then
+                        fancy_message sub $"%b [required]" "${CYAN}${onlyreal}${NC} ${RED}✗${NC}"
+                        echo "${onlyreal}" >> "${onlywhere}"
+                        return 0
+                    fi
+                fi
+            fi
+        fi
+    else
+        if [[ -z "$(apt-cache search --no-generate --names-only "^${onlyname}\$" 2> /dev/null || apt-cache search --names-only "^${onlyname}\$")" ]]; then
+            if [[ -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname})?architecture(${onlyarch})" -F "%p")" ]]; then
+                if [[ -z "$(aptitude search --quiet --disable-columns "?exact-name(${onlyname})?architecture(all)" -F "%p")" ]]; then
+                    if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname}$)?architecture(${onlyarch})" -F "%p")" ]]; then
+                        if [[ -z "$(aptitude search --quiet --disable-columns "?provides(^${onlyname}$)?architecture(all)" -F "%p")" ]]; then
+                            fancy_message sub $"%b [required]" "${CYAN}${onlyreal}${NC} ${RED}✗${NC}"
+                            echo "${onlyreal}" >> "${onlywhere}"
+                            return 0
+                        fi
+                    fi
+                fi
+            fi
+        fi
     fi
 }
 
